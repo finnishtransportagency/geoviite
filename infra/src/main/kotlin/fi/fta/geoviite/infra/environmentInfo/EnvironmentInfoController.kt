88b34b9a2b0dc47437e519bd7cf56acfba8a8fd4
Tiandu Zhao <tiandu.zhao@solita.fi>
2022-11-08 14:49:58 +0200

    GVT-1465: New api for fetching environment settings

---
+++ infra/src/main/kotlin/fi/fta/geoviite/infra/environmentInfo/EnvironmentInfoController.kt
@@ -0,0 +1,25 @@
+package fi.fta.geoviite.infra.environmentInfo
+
+import fi.fta.geoviite.infra.authorization.AUTH_ALL_READ
+import fi.fta.geoviite.infra.logging.apiCall
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+
+@RestController
+@RequestMapping("/environment")
+class EnvironmentInfoController @Autowired constructor(val info: EnvironmentInfo) {
+    private val logger: Logger = LoggerFactory.getLogger(this::class.java)
+
+    @GetMapping
+    @PreAuthorize(AUTH_ALL_READ)
+    fun getEnvironmentInfo(): EnvironmentInfo {
+        logger.apiCall("getEnvironmentInfo")
+        return info
+    }
+}
