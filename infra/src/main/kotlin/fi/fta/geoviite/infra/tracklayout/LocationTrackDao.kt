913aeac1afd8ced1e676758ebc99447444b21cba
Ari Rahikkala <ari.rahikkala@solita.fi>
2022-11-25 12:56:10 +0200

    GVT-1489 Filter out deleted location tracks from duplicate list

--- infra/src/main/kotlin/fi/fta/geoviite/infra/tracklayout/LocationTrackDao.kt
+++ infra/src/main/kotlin/fi/fta/geoviite/infra/tracklayout/LocationTrackDao.kt
@@ -32,6 +32,7 @@ class LocationTrackDao(jdbcTemplateParam: NamedParameterJdbcTemplate?)
             from layout.location_track_publication_view
             where duplicate_of_location_track_id = :id
               and :publishType = any(publication_states)
+              and state != 'DELETED'
         """.trimIndent()
         val params = mapOf("id" to id.intValue, "publishType" to publishType.name)
         val locationTracks = jdbcTemplate.query(sql, params) { rs, _ ->
