68abb4a7968df7c9aa4e28d144ebf7f2326f477a
Juuse Koivula <juuse.koivula@solita.fi>
2022-11-28 09:53:00 +0200

    GVT-1493: Cherry-pick BOM parsing changes

--- infra/src/test/kotlin/fi/fta/geoviite/infra/inframodel/InfraModelParsingIT.kt
+++ infra/src/test/kotlin/fi/fta/geoviite/infra/inframodel/InfraModelParsingIT.kt
@@ -40,14 +40,33 @@ class InfraModelParsingIT @Autowired constructor(
 
     @Test
     fun importingBOMFileWithISOXmlEncodingWorks() {
-        val testFileWithBom = (ByteOrderMark.UTF_BOM + "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>").toByteArray()
+        val isoTestFileWithBom = (ByteOrderMark.UTF_BOM + "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>").toByteArray()
+        assertDoesNotThrow { xmlBytesToString(isoTestFileWithBom) }
+    }
+
+    @Test
+    fun importingUTF16FileWithBOMWorks() {
+        val UTF16file = ByteOrderMark.UTF_16BE.bytes +
+                "<?xml version=\"1.0\" encoding=\"UTF-16\"?>".toByteArray(charset = Charsets.UTF_16BE)
+        assertDoesNotThrow { xmlBytesToString(UTF16file) }
+    }
+
+    @Test
+    fun importingUSASCIIFileWorks() {
+        val asciiTestFile = "<?xml version=\"1.0\" encoding=\"ASCII\"?>".toByteArray(charset = Charsets.US_ASCII)
+        assertDoesNotThrow { xmlBytesToString(asciiTestFile) }
+    }
+
+    @Test
+    fun importingUTF8FileWorks() {
+        val testFileWithBom = (ByteOrderMark.UTF_BOM + "<?xml version=\"1.0\" encoding=\"UTF-8\"?>").toByteArray()
         assertDoesNotThrow { xmlBytesToString(testFileWithBom) }
     }
 
     @Test
-    fun importingBOMlessFileWorks() {
-        val testFileWithBom = "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>".toByteArray()
-        assertDoesNotThrow { xmlBytesToString(testFileWithBom) }
+    fun importingISOFileWorks() {
+        val isoFile = "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>".toByteArray()
+        assertDoesNotThrow { xmlBytesToString(isoFile) }
     }
 
     @Test
