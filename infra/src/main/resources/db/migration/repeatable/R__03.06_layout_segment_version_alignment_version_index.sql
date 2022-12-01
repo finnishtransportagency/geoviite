71e690e4143605e5a55b27613664d077d1fd8dec
Jyrki Jakobsson <jyrki.jakobsson@solita.fi>
2022-12-01 18:32:44 +0200

    GVT-1535 Make alignment-fetch respect versioning + add indexing to parent version for segment & switch joint

---
+++ infra/src/main/resources/db/migration/repeatable/R__03.06_layout_segment_version_alignment_version_index.sql
@@ -0,0 +1,2 @@
+drop index if exists layout.layout_segment_version_alignment_version_index;
+create index layout_segment_version_alignment_version_index on layout.segment_version(alignment_id, alignment_version);
