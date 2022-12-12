61a0e7c6ebebb655c589df9a87b92345abe893d6
Juuse Koivula <juuse.koivula@solita.fi>
2022-12-12 14:22:58 +0200

    GVT-1551: Split publication-table into separate preview and publication tables

---
+++ ui/src/preview/change-table-entry-mapping.ts
@@ -0,0 +1,133 @@
+import {
+    KmPostPublishCandidate,
+    LocationTrackPublishCandidate,
+    Operation,
+    PublishCandidates,
+    ReferenceLinePublishCandidate,
+    SwitchPublishCandidate,
+    TrackNumberPublishCandidate,
+} from 'publication/publication-model';
+import { TimeStamp } from 'common/common-model';
+import { SelectedPublishChange } from 'track-layout/track-layout-store';
+import {
+    LayoutKmPostId,
+    LayoutSwitchId,
+    LayoutTrackNumber,
+    LayoutTrackNumberId,
+    LocationTrackId,
+    ReferenceLineId,
+} from 'track-layout/track-layout-model';
+
+export type ChangeTableProps = {
+    previewChanges: PublishCandidates;
+    showRatkoPushDate?: boolean;
+    showStatus?: boolean;
+    showActions?: boolean;
+    ratkoPushDate?: TimeStamp;
+    onPreviewSelect?: (selectedChanges: SelectedPublishChange) => void;
+    publish?: boolean;
+};
+
+type PublicationId =
+    | LayoutTrackNumberId
+    | ReferenceLineId
+    | LocationTrackId
+    | LayoutSwitchId
+    | LayoutKmPostId;
+
+export type ChangeTableEntry = {
+    id: PublicationId;
+    name: string;
+    uiName: string;
+    trackNumber: string;
+    changeTime: string;
+    userName: string;
+    operation: Operation;
+};
+
+type TranslationFunc = (tKey: string) => string;
+
+export const trackNumberToChangeTableEntry = (
+    trackNumber: TrackNumberPublishCandidate,
+    t: TranslationFunc,
+) => ({
+    id: trackNumber.id,
+    uiName: `${t('publication-table.track-number-long')} ${trackNumber.number}`,
+    name: trackNumber.number,
+    userName: trackNumber.userName,
+    trackNumber: trackNumber.number,
+    changeTime: trackNumber.draftChangeTime,
+    operation: trackNumber.operation,
+});
+
+export const referenceLineToChangeTableEntry = (
+    referenceLine: ReferenceLinePublishCandidate,
+    trackNumbers: LayoutTrackNumber[],
+    t: TranslationFunc,
+) => {
+    const trackNumber = trackNumbers.find((tn) => tn.id === referenceLine.trackNumberId);
+    return {
+        id: referenceLine.id,
+        uiName: `${t('publication-table.reference-line')} ${referenceLine.name}`,
+        name: referenceLine.name,
+        userName: referenceLine.userName,
+        trackNumber: trackNumber ? trackNumber.number : '',
+        changeTime: referenceLine.draftChangeTime,
+        operation: referenceLine.operation,
+    };
+};
+
+export const locationTrackToChangeTableEntry = (
+    locationTrack: LocationTrackPublishCandidate,
+    trackNumbers: LayoutTrackNumber[],
+    t: TranslationFunc,
+) => {
+    const trackNumber = trackNumbers.find((tn) => tn.id === locationTrack.trackNumberId);
+    return {
+        id: locationTrack.id,
+        uiName: `${t('publication-table.location-track')} ${locationTrack.name}`,
+        name: locationTrack.name,
+        userName: locationTrack.userName,
+        trackNumber: trackNumber ? trackNumber.number : '',
+        changeTime: locationTrack.draftChangeTime,
+        operation: locationTrack.operation,
+    };
+};
+
+export const switchToChangeTableEntry = (
+    layoutSwitch: SwitchPublishCandidate,
+    trackNumbers: LayoutTrackNumber[],
+    t: TranslationFunc,
+) => {
+    const trackNumber = trackNumbers
+        .filter((tn) => layoutSwitch.trackNumberIds.some((lstn) => lstn == tn.id))
+        .sort()
+        .map((tn) => tn.number)
+        .join(', ');
+    return {
+        id: layoutSwitch.id,
+        uiName: `${t('publication-table.switch')} ${layoutSwitch.name}`,
+        name: layoutSwitch.name,
+        userName: layoutSwitch.userName,
+        trackNumber,
+        changeTime: layoutSwitch.draftChangeTime,
+        operation: layoutSwitch.operation,
+    };
+};
+
+export const kmPostChangeTableEntry = (
+    kmPost: KmPostPublishCandidate,
+    trackNumbers: LayoutTrackNumber[],
+    t: TranslationFunc,
+) => {
+    const trackNumber = trackNumbers.find((tn) => tn.id === kmPost.trackNumberId);
+    return {
+        id: kmPost.id,
+        uiName: `${t('publication-table.km-post')} ${kmPost.kmNumber}`,
+        name: kmPost.kmNumber,
+        userName: kmPost.userName,
+        trackNumber: trackNumber ? trackNumber.number : '',
+        changeTime: kmPost.draftChangeTime,
+        operation: kmPost.operation,
+    };
+};
--- ui/src/publication/publication-table-item.tsx
+++ ui/src/publication/publication-table-item.tsx
@@ -1,57 +1,27 @@
 import * as React from 'react';
 import { TimeStamp } from 'common/common-model';
-import styles from 'publication/publication-table-item.scss';
-import { IconColor, Icons, IconSize } from 'vayla-design-lib/icon/Icon';
 import { formatDateFull } from 'utils/date-utils';
 import { useTranslation } from 'react-i18next';
-import { Operation, PublishValidationError } from 'publication/publication-model';
-import { createClassName } from 'vayla-design-lib/utils';
+import { Operation } from 'publication/publication-model';
 
 export type PreviewTableItemProps = {
     itemName: string;
     trackNumber?: string;
-    errors: PublishValidationError[];
     changeTime: TimeStamp;
-    showRatkoPushDate: boolean;
-    showStatus: boolean;
-    showActions: boolean;
     ratkoPushDate?: TimeStamp;
     operation: Operation | null;
     userName: string;
-    onPublishItemSelect?: () => void;
-    publish?: boolean;
 };
 
 export const PublicationTableItem: React.FC<PreviewTableItemProps> = ({
     itemName,
     trackNumber,
-    errors,
     changeTime,
-    showRatkoPushDate,
-    showStatus,
-    showActions,
     ratkoPushDate,
     operation,
     userName,
-    onPublishItemSelect,
-    publish = false,
 }) => {
     const { t } = useTranslation();
-    const [isErrorRowExpanded, setIsErrorRowExpanded] = React.useState(false);
-
-    const errorsToStrings = (list: PublishValidationError[], type: 'ERROR' | 'WARNING') => {
-        const filtered = list.filter((e) => e.type === type);
-        return filtered.map((error) => t(error.localizationKey, error.params));
-    };
-
-    const errorTexts = errorsToStrings(errors, 'ERROR');
-    const warningTexts = errorsToStrings(errors, 'WARNING');
-    const hasErrors = errors.length > 0;
-
-    const statusCellClassName = createClassName(
-        styles['preview-table-item__status-cell'],
-        hasErrors && styles['preview-table-item__status-cell--expandable'],
-    );
 
     return (
         <React.Fragment>
@@ -61,73 +31,8 @@ export const PublicationTableItem: React.FC<PreviewTableItemProps> = ({
                 <td>{operation ? t(`enum.publish-operation.${operation}`) : ''}</td>
                 <td>{formatDateFull(changeTime)}</td>
                 <td>{userName}</td>
-                {showStatus && (
-                    <td
-                        className={statusCellClassName}
-                        onClick={() => setIsErrorRowExpanded(!isErrorRowExpanded)}>
-                        {!hasErrors && (
-                            <span className={styles['preview-table-item__ok-status']}>
-                                <Icons.Tick color={IconColor.INHERIT} size={IconSize.SMALL} />
-                            </span>
-                        )}
-                        {errorTexts.length > 0 && (
-                            <span className={styles['preview-table-item__error-status']}>
-                                {t('publication-table.errors-status-text', [errorTexts.length])}
-                            </span>
-                        )}
-                        {warningTexts.length > 0 && (
-                            <span className={styles['preview-table-item__warning-status']}>
-                                {t('publication-table.warnings-status-text', [warningTexts.length])}
-                            </span>
-                        )}
-                    </td>
-                )}
-                {showRatkoPushDate && (
-                    <td>{ratkoPushDate ? formatDateFull(ratkoPushDate) : t('no')}</td>
-                )}
-                {showActions && (
-                    <td
-                        onClick={() => {
-                            onPublishItemSelect && onPublishItemSelect();
-                        }}>
-                        {publish ? (
-                            <Icons.Ascending size={IconSize.SMALL} />
-                        ) : (
-                            <Icons.Descending size={IconSize.SMALL} />
-                        )}
-                    </td>
-                )}
+                <td>{ratkoPushDate ? formatDateFull(ratkoPushDate) : t('no')}</td>
             </tr>
-            {isErrorRowExpanded && hasErrors && (
-                <tr className={'preview-table-item preview-table-item--error'}>
-                    <td colSpan={4}>
-                        {errorTexts.length > 0 && (
-                            <div className="preview-table-item__msg-group preview-table-item__msg-group--errors">
-                                <div className="preview-table-item__group-title">
-                                    {t('publication-table.errors-group-title')}
-                                </div>
-                                {errorTexts.map((errorText, index) => (
-                                    <div key={index} className="preview-table-item__msg">
-                                        {errorText}
-                                    </div>
-                                ))}
-                            </div>
-                        )}
-                        {warningTexts.length > 0 && (
-                            <div className="preview-table-item__msg-group preview-table-item__msg-group--warnings">
-                                <div className="preview-table-item__group-title">
-                                    {t('publication-table.warnings-group-title')}
-                                </div>
-                                {warningTexts?.map((warningText, index) => (
-                                    <div key={index} className="preview-table-item__msg">
-                                        {warningText}
-                                    </div>
-                                ))}
-                            </div>
-                        )}
-                    </td>
-                </tr>
-            )}
         </React.Fragment>
     );
 };
