c4979aa8983a2f6c53f2cf88dcd29a998bf8be24
Tiandu Zhao <tiandu.zhao@solita.fi>
2022-12-19 16:16:41 +0200

    GVT-1559: Snap measurement tool to nearby points

--- ui/src/map/tools/measurement-tool.ts
+++ ui/src/map/tools/measurement-tool.ts
@@ -4,10 +4,14 @@ import OlMap from 'ol/Map';
 import { Stroke, Style } from 'ol/style';
 import { Precision, roundToPrecision } from 'utils/rounding';
 import Overlay from 'ol/Overlay';
-import { EventsKey } from 'ol/events';
-import { unByKey } from 'ol/Observable';
 import mapStyles from '../map.module.scss';
 import CircleStyle from 'ol/style/Circle';
+import { LineString } from 'ol/geom';
+import { Coordinate } from 'ol/coordinate';
+import { getPlanarDistanceUnwrapped, SegmentDataHolder } from 'map/layers/layer-utils';
+import { filterNotEmpty } from 'utils/array-utils';
+import { LayoutPoint } from 'track-layout/track-layout-model';
+import { FEATURE_PROPERTY_SEGMENT_DATA } from 'map/layers/alignment-layer';
 
 function formatMeasurement(distance: number) {
     let content;
@@ -21,6 +25,42 @@ function formatMeasurement(distance: number) {
 
     return content;
 }
+
+function getClosestPoints(
+    points: LayoutPoint[],
+    targetCoordinate: number[],
+    maxPoints: number,
+): LayoutPoint[] {
+    if (points.length <= maxPoints) {
+        return points;
+    } else {
+        const firstPointDistance = getPlanarDistanceUnwrapped(
+            targetCoordinate[0],
+            targetCoordinate[1],
+            points[0].x,
+            points[0].y,
+        );
+
+        const lastPointDistance = getPlanarDistanceUnwrapped(
+            targetCoordinate[0],
+            targetCoordinate[1],
+            points[points.length - 1].x,
+            points[points.length - 1].y,
+        );
+
+        const middleIndex = Math.floor(points.length / 2);
+
+        const newPoints =
+            firstPointDistance < lastPointDistance
+                ? points.slice(0, middleIndex)
+                : points.slice(middleIndex);
+
+        return getClosestPoints(newPoints, targetCoordinate, maxPoints);
+    }
+}
+
+//Tolerance for snapping, in pixels
+const hitTolerance = 8;
 
 export const measurementTool: MapTool = {
     activate: (map: OlMap) => {
@@ -52,24 +92,59 @@ export const measurementTool: MapTool = {
                     }),
                 }),
             }),
-        });
+            geometryFunction: function (coordinates: Coordinate[], existingGeom: LineString) {
+                const cursorCoordinate = coordinates[coordinates.length - 1];
+                const cursorPixel = map.getPixelFromCoordinate(cursorCoordinate);
+                const nearbyFeatures = map.getFeaturesAtPixel(cursorPixel, { hitTolerance });
 
-        let geometryListener: EventsKey | undefined;
-        tooltipDraw.on('drawstart', function (evt) {
-            geometryListener = evt.feature.getGeometry()?.on('change', function (evt) {
-                const geom = evt.target;
-                const distance = geom.getLength();
+                const nearbyPoints = nearbyFeatures
+                    .map((f) => f.get(FEATURE_PROPERTY_SEGMENT_DATA))
+                    .filter(filterNotEmpty)
+                    .flatMap((f: SegmentDataHolder) =>
+                        getClosestPoints(f.segment.points, cursorCoordinate, 8),
+                    );
 
-                tooltipElement.innerHTML = formatMeasurement(distance);
-                tooltip.setPosition(geom.getLastCoordinate());
-            });
+                let closestPoint;
+                for (let i = 0; i < nearbyPoints.length; i++) {
+                    const nearbyPoint = nearbyPoints[i];
+                    const pixelPoint = map.getPixelFromCoordinate([nearbyPoint.x, nearbyPoint.y]);
+
+                    const distance = Math.hypot(
+                        cursorPixel[0] - pixelPoint[0],
+                        cursorPixel[1] - pixelPoint[1],
+                    );
+
+                    if (!closestPoint || distance < closestPoint.distance) {
+                        closestPoint = {
+                            distance: distance,
+                            point: nearbyPoint,
+                        };
+                    }
+                }
+
+                const newCoordinates =
+                    closestPoint && closestPoint.distance < hitTolerance
+                        ? [closestPoint.point.x, closestPoint.point.y]
+                        : cursorCoordinate;
+
+                if (existingGeom) {
+                    existingGeom.setCoordinates([
+                        existingGeom.getFirstCoordinate(),
+                        newCoordinates,
+                    ]);
+                } else {
+                    existingGeom = new LineString([newCoordinates]);
+                }
+
+                tooltipElement.innerHTML = formatMeasurement(existingGeom.getLength());
+                tooltip.setPosition(existingGeom.getLastCoordinate());
+
+                return existingGeom;
+            },
         });
 
         tooltipDraw.on('drawend', function () {
             tooltip.setPosition(undefined);
-            if (geometryListener) {
-                unByKey(geometryListener);
-            }
         });
 
         map.addInteraction(tooltipDraw);
