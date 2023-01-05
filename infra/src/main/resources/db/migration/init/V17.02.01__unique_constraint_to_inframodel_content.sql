6eb44032639d7149bbf72b25227f583e57fd8b70
Taru Saloheimo <taru.saloheimo@solita.fi>
2023-01-05 15:58:38 +0200

    GVT-821 Make it impossible to download a duplicate IM file

--- infra/src/main/resources/db/migration/init/V17.02.01__unique_constraint_to_inframodel_content.sql
+++ infra/src/main/resources/db/migration/init/V17.02.01__unique_constraint_to_inframodel_content.sql
@@ -1,11 +1,3 @@
 alter table geometry.plan_file
-  add hash text;
-
-alter table geometry.plan_file
-  add hash_testi varchar(200);
-
-
-update geometry.plan_file set hash_testi = 'olen jännittävä arvo' where plan_file.hash_testi is null;
-
-update geometry.plan_file set hash =md5(cast((geometry.plan_file.content) as text));
+  add hash text unique generated always as (md5(content::text)) stored;
 
