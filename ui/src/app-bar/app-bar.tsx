bea714f96e9d4b9cd5509b1c13ea6e46f1206ab3
Juuse Koivula <juuse.koivula@solita.fi>
2023-01-18 13:22:10 +0200

    GVT-1632: Add a data products menu to the front page and translate app-bar

--- ui/src/app-bar/app-bar.tsx
+++ ui/src/app-bar/app-bar.tsx
@@ -5,6 +5,8 @@ import geoviiteLogo from 'geoviite-design-lib/geoviite-logo.svg';
 import vaylaLogo from 'vayla-design-lib/logo/vayla-logo.svg';
 import { EnvRestricted } from 'environment/env-restricted';
 import { Environment } from 'environment/environment-info';
+import { Menu } from 'vayla-design-lib/menu/menu';
+import { useTranslation } from 'react-i18next';
 
 type Link = {
     link: string;
@@ -13,15 +15,28 @@ type Link = {
 };
 
 const links: Link[] = [
-    { link: '/', name: 'Etusivu', type: 'prod' },
-    { link: '/track-layout', name: 'Kartta', type: 'prod' },
-    { link: '/rekisteri', name: 'Rekisteri', type: 'prod' },
-    { link: '/infra-model', name: 'InfraModel', type: 'prod' },
-    { link: '/design-lib-demo', name: 'Components', type: 'dev' },
-    { link: '/localization-demo', name: 'Localization', type: 'dev' },
+    { link: '/', name: 'app-bar.frontpage', type: 'prod' },
+    { link: '/track-layout', name: 'app-bar.track-layout', type: 'prod' },
+    { link: '/rekisteri', name: 'app-bar.register', type: 'prod' },
+    { link: '/infra-model', name: 'app-bar.infra-model', type: 'prod' },
+    { link: '/design-lib-demo', name: 'app-bar.components', type: 'dev' },
+    { link: '/localization-demo', name: 'app-bar.localization', type: 'dev' },
 ];
 
 export const AppBar: React.FC = () => {
+    const { t } = useTranslation();
+    const [dataMenuOpen, setDataMenuOpen] = React.useState(false);
+
+    const dataMenuItems = [
+        { value: 'MENU1', name: t('app-bar.data-products.element-list') },
+        { value: 'MENU2', name: t('app-bar.data-products.vertical-geometry') },
+        { value: 'MENU3', name: t('app-bar.data-products.km-lengths') },
+    ];
+
+    const handleItemChange = (_item: string) => {
+        // TODO add navigation to subpages
+    };
+
     return (
         <nav className={styles['app-bar']}>
             <div className={styles['app-bar__title']}>
@@ -42,12 +57,28 @@ export const AppBar: React.FC = () => {
                                             }`
                                         }
                                         end>
-                                        {link.name}
+                                        {t(link.name)}
                                     </NavLink>
                                 </li>
                             </EnvRestricted>
                         );
                     })}
+                <li>
+                    <EnvRestricted restrictTo={'dev'}>
+                        <div
+                            className={`${styles['app-bar__link']} ${styles['app-bar__data-menu-button']}`}
+                            onClick={() => setDataMenuOpen(!dataMenuOpen)}>
+                            <span>{t('app-bar.data-products-title')}</span>
+                            {dataMenuOpen && (
+                                <div className={styles['app-bar__data-menu']}>
+                                    <Menu
+                                        items={dataMenuItems}
+                                        onChange={(item) => item && handleItemChange(item)}></Menu>
+                                </div>
+                            )}
+                        </div>
+                    </EnvRestricted>
+                </li>
             </ul>
             <img
                 className={styles['app-bar__vayla-logo']}
