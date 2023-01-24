0ae9ce8ccef2772ba8c8830dbd92002ad5558ace
Juuse Koivula <juuse.koivula@solita.fi>
2023-01-24 10:47:38 +0200

    GVT-1646: Add a button and filtering logic for only showing the current user's unstaged changes in preview-view

--- ui/src/preview/preview-view.tsx
+++ ui/src/preview/preview-view.tsx
@@ -47,6 +47,9 @@ import PreviewTable, { PreviewSelectType, PreviewTableEntry } from 'preview/prev
 import { updateAllChangeTimes } from 'common/change-time-api';
 import * as Snackbar from 'geoviite-design-lib/snackbar/snackbar';
 import { PreviewConfirmRevertChangesDialog } from 'preview/preview-confirm-revert-changes-dialog';
+import { Checkbox } from 'vayla-design-lib/checkbox/checkbox';
+import { User } from 'user/user-model';
+import { getOwnUser } from 'user/user-api';
 
 type CandidateId =
     | LocationTrackId
@@ -69,6 +72,8 @@ export type SelectedChanges = {
 type PendingValidation = {
     pendingValidation: boolean;
 };
+
+type PreviewCandidate = PublishCandidate & PendingValidation;
 
 export type PreviewCandidates = {
     trackNumbers: (TrackNumberPublishCandidate & PendingValidation)[];
@@ -269,6 +274,20 @@ const singleRowPublishRequestOfSelectedPublishChange = (
     kmPosts: change.kmPost ? [change.kmPost] : [],
 });
 
+const filterCandidatesByUser = <T extends PreviewCandidate>(user: User, candidates: T[]) =>
+    candidates.filter((candidate) => candidate.userName === user.details.userName);
+
+const filterChangeSetByUser = (
+    user: User,
+    publishCandidates: PreviewCandidates,
+): PreviewCandidates => ({
+    trackNumbers: filterCandidatesByUser(user, publishCandidates.trackNumbers),
+    referenceLines: filterCandidatesByUser(user, publishCandidates.referenceLines),
+    locationTracks: filterCandidatesByUser(user, publishCandidates.locationTracks),
+    switches: filterCandidatesByUser(user, publishCandidates.switches),
+    kmPosts: filterCandidatesByUser(user, publishCandidates.kmPosts),
+});
+
 export const PreviewView: React.FC<PreviewProps> = (props: PreviewProps) => {
     const { t } = useTranslation();
 
@@ -281,6 +300,8 @@ export const PreviewView: React.FC<PreviewProps> = (props: PreviewProps) => {
     // instead of relying on React's dependency-based redraw.
     const [changeTableUpdateToken, setChangeTableUpdateToken] = React.useState<number>();
     const updateChangeTables = () => setChangeTableUpdateToken(Date.now());
+    const [onlyShowMine, setOnlyShowMine] = React.useState(false);
+    const user = useLoader(getOwnUser, []);
 
     const entireChangeset = useLoader(() => getPublishCandidates(), [props.changeTimes]);
     const validatedChangeset = useLoader(
@@ -291,7 +312,7 @@ export const PreviewView: React.FC<PreviewProps> = (props: PreviewProps) => {
                 updateChangeTables();
                 return validatedPublishCandidates;
             }),
-        [entireChangeset],
+        [entireChangeset, onlyShowMine],
     );
     const unstagedChanges = validatedChangeset
         ? getUnstagedChanges(
@@ -306,19 +327,20 @@ export const PreviewView: React.FC<PreviewProps> = (props: PreviewProps) => {
           )
         : undefined;
 
-    const unstagedPreviewChanges: PreviewCandidates = React.useMemo(
-        () =>
-            unstagedChanges
-                ? {
-                      trackNumbers: unstagedChanges.trackNumbers.map(nonPendingCandidate),
-                      referenceLines: unstagedChanges.referenceLines.map(nonPendingCandidate),
-                      locationTracks: unstagedChanges.locationTracks.map(nonPendingCandidate),
-                      switches: unstagedChanges.switches.map(nonPendingCandidate),
-                      kmPosts: unstagedChanges.kmPosts.map(nonPendingCandidate),
-                  }
-                : emptyChanges,
-        [changeTableUpdateToken],
-    );
+    const unstagedPreviewChanges: PreviewCandidates = React.useMemo(() => {
+        const allUnstagedChangesValidated = unstagedChanges
+            ? {
+                  trackNumbers: unstagedChanges.trackNumbers.map(nonPendingCandidate),
+                  referenceLines: unstagedChanges.referenceLines.map(nonPendingCandidate),
+                  locationTracks: unstagedChanges.locationTracks.map(nonPendingCandidate),
+                  switches: unstagedChanges.switches.map(nonPendingCandidate),
+                  kmPosts: unstagedChanges.kmPosts.map(nonPendingCandidate),
+              }
+            : emptyChanges;
+        return user && onlyShowMine
+            ? filterChangeSetByUser(user, allUnstagedChangesValidated)
+            : allUnstagedChangesValidated;
+    }, [changeTableUpdateToken, onlyShowMine]);
 
     const stagedPreviewChanges: PreviewCandidates = React.useMemo(
         () =>
@@ -396,6 +418,11 @@ export const PreviewView: React.FC<PreviewProps> = (props: PreviewProps) => {
                                 className={styles['preview-section']}>
                                 <div className={styles['preview-view__changes-title']}>
                                     <h3>{t('preview-view.unstaged-changes-title')}</h3>
+                                    <Checkbox
+                                        checked={onlyShowMine}
+                                        onChange={(e) => setOnlyShowMine(e.target.checked)}>
+                                        {t('preview-view.show-only-mine')}
+                                    </Checkbox>
                                 </div>
                                 <PreviewTable
                                     onPreviewSelect={onPreviewSelect}
