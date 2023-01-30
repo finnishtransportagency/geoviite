2f97f2e36d6c1d12010637ce553ddc0e1b5979b8
Ari Rahikkala <ari.rahikkala@solita.fi>
2023-01-30 16:09:03 +0200

    GVT-1469 Update SecurityConfiguration to not use deprecated features

--- infra/src/main/kotlin/fi/fta/geoviite/infra/configuration/SecurityConfiguration.kt
+++ infra/src/main/kotlin/fi/fta/geoviite/infra/configuration/SecurityConfiguration.kt
@@ -3,37 +3,36 @@ package fi.fta.geoviite.infra.configuration
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy.STATELESS
+import org.springframework.security.web.SecurityFilterChain
+
 
 @ConditionalOnWebApplication
 @EnableWebSecurity
 @Configuration
-class SecurityConfiguration : WebSecurityConfigurerAdapter(true) {
-
+class SecurityConfiguration {
     val logger: Logger = LoggerFactory.getLogger(this::class.java)
 
-    override fun configure(auth: AuthenticationManagerBuilder) {
-        logger.info("Configuring authentication manager")
-        auth.inMemoryAuthentication() // No users
-    }
-
-    override fun configure(http: HttpSecurity) {
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
         logger.info("Configuring HTTP Security")
-        val httpConfig = http
+        return http
             .httpBasic().disable()
             // Disable CORS: Not sharing anything with other domains
             .cors().disable()
             // Disable CSRF: JWT is stored in ALB -> session not configured here
             .csrf().disable()
+            // Disable SecurityContextPersistenceFilter which by default would replace the security context set up in
+            // RequestFilter with an empty one
+            .securityContext().disable()
             // Set session management to stateless
             .sessionManagement().sessionCreationPolicy(STATELESS).and()
-
-        // Set permissions on endpoints
-        httpConfig.authorizeRequests().anyRequest().authenticated().and()
+            // Set permissions on endpoints
+            .authorizeHttpRequests().anyRequest().authenticated().and()
+            .build()
     }
 }
