3ca551d78c47a28fea228bbc3ff6bb8c5e710c61
Taru Saloheimo <taru.saloheimo@solita.fi>
2023-02-27 14:09:03 +0200

    GVT-1731 Refactor how url is connected to component

--- ui/src/data-products/data-products-container-with-provider.tsx
+++ ui/src/data-products/data-products-container-with-provider.tsx
@@ -4,17 +4,42 @@ import React from 'react';
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistStore } from 'redux-persist';
 import ElementListView from 'data-products/element-list/element-list-view';
+import VerticalGeometryView from 'data-products/vertical-geometry/vertical-geometry-view';
 
 const persistorDp = persistStore(dataProductsStore);
 
-const DataProductsMainContainer = connect()(ElementListView);
+type SelectedDataProduct = 'ELEMENT_LIST' | 'VERTICAL_GEOMETRY';
 
-export const DataProductsMainContainerWithProvider: React.FC = () => {
-    return (
-        <Provider store={dataProductsStore}>
-            <PersistGate loading={null} persistor={persistorDp}>
-                <DataProductsMainContainer />
-            </PersistGate>
-        </Provider>
-    );
+export type DataProductsContainerWithProviderProps = {
+    selectedDataProduct: SelectedDataProduct;
+};
+
+const DataProductsMainContainerElementListView = connect()(ElementListView);
+const DataProductsMainContainerVerticalGeometryView = connect()(VerticalGeometryView);
+
+function renderPath(products: SelectedDataProduct) {
+    switch (products) {
+        case 'ELEMENT_LIST':
+            return (
+                <Provider store={dataProductsStore}>
+                    <PersistGate loading={null} persistor={persistorDp}>
+                        <DataProductsMainContainerElementListView />
+                    </PersistGate>
+                </Provider>
+            );
+        case 'VERTICAL_GEOMETRY':
+            return (
+                <Provider store={dataProductsStore}>
+                    <PersistGate loading={null} persistor={persistorDp}>
+                        <DataProductsMainContainerVerticalGeometryView />
+                    </PersistGate>
+                </Provider>
+            );
+    }
+}
+
+export const DataProductsMainContainerWithProvider: React.FC<
+    DataProductsContainerWithProviderProps
+> = ({ selectedDataProduct }) => {
+    return renderPath(selectedDataProduct);
 };
