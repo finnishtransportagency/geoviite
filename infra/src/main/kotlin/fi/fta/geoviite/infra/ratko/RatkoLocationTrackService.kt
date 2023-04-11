4f78f2048d2d10c793fb017efa575153b7e2d0c3
Ari Rahikkala <ari.rahikkala@solita.fi>
2023-04-11 14:22:52 +0300

    GVT-1809 Avoid rounding error in filtering short metadatas
    The isSame check uses HALF_UP rounding at each end, which can for
    instance see a distance from 0 to 0.6 as being a full meter
    long. Moving the actual check to after any rounding makes it easier to
    ensure that we are matching the check that Ratko does.

--- infra/src/main/kotlin/fi/fta/geoviite/infra/ratko/RatkoLocationTrackService.kt
+++ infra/src/main/kotlin/fi/fta/geoviite/infra/ratko/RatkoLocationTrackService.kt
@@ -157,8 +157,6 @@ class RatkoLocationTrackService @Autowired constructor(
                     else geocodingContext.cutRangeByKms(origMetaDataRange, changedKmNumbers)
 
                     val splitMetaDataAssets = splitAddressRanges
-                        // Ignore metadata where the address range is under 1m, since there are no address points for it
-                        .filter { addressRange -> !addressRange.start.isSame(addressRange.endInclusive, 0) }
                         .mapNotNull { addressRange ->
                             val startPoint = findAddressPoint(alignmentPoints, addressRange.start, AddessRounding.UP)
                             val endPoint = findAddressPoint(alignmentPoints, addressRange.endInclusive, AddessRounding.DOWN)
@@ -168,8 +166,8 @@ class RatkoLocationTrackService @Autowired constructor(
                                 endPoint = endPoint.point.toPoint(),
                             )
 
-                            // If we only have 1 point in the interval, don't send it as it covers no length
-                            if (startPoint.address < endPoint.address) convertToRatkoMetadataAsset(
+                            // Ignore metadata where the address range is under 1m, since there are no address points for it
+                            if (startPoint.address + 1 <= endPoint.address) convertToRatkoMetadataAsset(
                                 trackNumberOid = trackNumberOid,
                                 locationTrackOid = layoutLocationTrack.externalId,
                                 segmentMetadata = splitMetaData,
