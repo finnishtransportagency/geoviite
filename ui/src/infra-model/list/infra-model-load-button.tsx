dea772a5a7976c3803982ea14440cc666deec45c
Taru Saloheimo <taru.saloheimo@solita.fi>
2023-04-17 20:48:06 +0300

    GVT-1729 Hide "Lataa uusi" button

--- ui/src/infra-model/list/infra-model-load-button.tsx
+++ ui/src/infra-model/list/infra-model-load-button.tsx
@@ -4,6 +4,8 @@ import { Icons } from 'vayla-design-lib/icon/Icon';
 import { Button } from 'vayla-design-lib/button/button';
 import { convertToSerializableFile, SerializableFile } from 'utils/file-utils';
 import { useAppNavigate } from 'common/navigate';
+import { useCommonDataAppSelector } from 'store/hooks';
+import { useTranslation } from 'react-i18next';
 
 export type InframodelLoadButtonProps = {
     onFileSelected: (file: SerializableFile) => void;
@@ -13,6 +15,8 @@ const InfraModelLoadButton: React.FC<InframodelLoadButtonProps> = ({
     onFileSelected,
 }: InframodelLoadButtonProps) => {
     const navigate = useAppNavigate();
+    const { t } = useTranslation();
+    const userHasWriteRole = useCommonDataAppSelector((state) => state.userHasWriteRole);
     async function handleFileInputEvent(e: React.ChangeEvent<HTMLInputElement>) {
         if (e.target.files) {
             const serializableFile = await convertToSerializableFile(e.target.files[0]);
@@ -24,7 +28,9 @@ const InfraModelLoadButton: React.FC<InframodelLoadButtonProps> = ({
     return (
         <React.Fragment>
             <FileInput onChange={(e) => handleFileInputEvent(e)} accept=".xml">
-                <Button icon={Icons.Append}>Lataa uusi</Button>
+                {userHasWriteRole && (
+                    <Button icon={Icons.Append}>{t('im-form.toolbar.upload')}</Button>
+                )}
             </FileInput>
         </React.Fragment>
     );
