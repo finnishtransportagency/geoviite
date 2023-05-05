9c471cca90656553d5b4fc470481615020df4c1b
Juuse Koivula <juuse.koivula@solita.fi>
2023-05-05 12:19:02 +0300

    GVT-1776: Remove unnecessary import

--- infra/src/main/kotlin/fi/fta/geoviite/infra/inframodel/InfraModelService.kt
+++ infra/src/main/kotlin/fi/fta/geoviite/infra/inframodel/InfraModelService.kt
@@ -20,7 +20,6 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
-import java.io.File
 
 const val VALIDATION_LAYOUT_POINTS_RESOLUTION = 10
 
