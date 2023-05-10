2fc75556367bb18ddc988d541c3145ba4f427bf9
Taru Saloheimo <taru.saloheimo@solita.fi>
2023-05-10 13:01:45 +0300

    GVT-1791 conflict

--- ui/src/infra-model/tabs/tabs.tsx
+++ ui/src/infra-model/tabs/tabs.tsx
@@ -5,7 +5,6 @@ import { useCommonDataAppSelector } from 'store/hooks';
 import { InfraModelListContainer } from 'infra-model/list/infra-model-list-container';
 import { useTranslation } from 'react-i18next';
 import { createDelegates } from 'store/store-utils';
-import { VelhoFileListContainer } from 'infra-model/velho/velho-file-list';
 import { infraModelActionCreators, InfraModelTabType } from 'infra-model/infra-model-slice';
 
 export type TabsProps = {
@@ -45,7 +44,7 @@ const Tabs: React.FC<TabsProps> = ({ activeTab: activeTab }) => {
                     <InfraModelListContainer changeTimes={changeTimes} />
                 </TabContent>
                 <TabContent tabId={InfraModelTabType.WAITING} activeTab={activeTab}>
-                    <VelhoFileListContainer />
+                    <p>VELHO-taulukko tähän</p>
                 </TabContent>
                 <TabContent tabId={InfraModelTabType.REJECTED} activeTab={activeTab}>
                     <p>Hello from rejected tab!</p>
