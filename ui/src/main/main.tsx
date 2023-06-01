ddf9e515a8e2c0c60576863e3243c8749a41b325
Taru Saloheimo <taru.saloheimo@solita.fi>
2023-06-01 13:35:02 +0300

    GVT-1297 add db password

--- ui/src/main/main.tsx
+++ ui/src/main/main.tsx
@@ -33,7 +33,7 @@ type MainProps = {
     layoutMode: LayoutMode;
     version: string | undefined;
 };
-//testaus
+
 const Main: React.VFC<MainProps> = (props: MainProps) => {
     const { t } = useTranslation();
     return (
