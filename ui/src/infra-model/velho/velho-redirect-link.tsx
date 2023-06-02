41212a47a37ef89c1871cd807809f951910b4ab1
Juuse Koivula <juuse.koivula@solita.fi>
2023-06-02 13:09:46 +0300

    GVT-1896: Add prefetching and caching to Velho redirect links

--- ui/src/infra-model/velho/velho-redirect-link.tsx
+++ ui/src/infra-model/velho/velho-redirect-link.tsx
@@ -3,23 +3,37 @@ import { Link } from 'vayla-design-lib/link/link';
 import styles from 'infra-model/velho/velho-file-list.scss';
 import { getVelhoRedirectUrl } from 'infra-model/infra-model-api';
 import { IconColor, Icons, IconSize } from 'vayla-design-lib/icon/Icon';
-import { Oid } from 'common/common-model';
+import { Oid, TimeStamp } from 'common/common-model';
+import { LoaderStatus, useLoaderWithStatus } from 'utils/react-utils';
+import { Spinner } from 'vayla-design-lib/spinner/spinner';
 
 type VelhoRedirectLinkProps = {
+    changeTime: TimeStamp;
     oid: Oid;
 } & React.HTMLProps<HTMLAnchorElement>;
 
-export const VelhoRedirectLink: React.FC<VelhoRedirectLinkProps> = ({ oid, children }) => {
+export const VelhoRedirectLink: React.FC<VelhoRedirectLinkProps> = ({
+    changeTime,
+    oid,
+    children,
+}) => {
+    const [url, loaderStatus] = useLoaderWithStatus(
+        () => getVelhoRedirectUrl(changeTime, oid),
+        [oid],
+    );
     return (
-        <Link
-            className={styles['velho-file-list__link']}
-            onClick={() =>
-                getVelhoRedirectUrl(oid).then((url) => {
-                    if (url) window.location.href = url;
-                })
-            }>
-            <span>{children}</span>
-            <Icons.ExternalLink color={IconColor.INHERIT} size={IconSize.SMALL} />
-        </Link>
+        <React.Fragment>
+            {loaderStatus === LoaderStatus.Ready ? (
+                <Link
+                    className={styles['velho-file-list__link']}
+                    href={url || undefined}
+                    target={'_blank'}>
+                    <span>{children}</span>
+                    <Icons.ExternalLink color={IconColor.INHERIT} size={IconSize.SMALL} />
+                </Link>
+            ) : (
+                <Spinner />
+            )}
+        </React.Fragment>
     );
 };
