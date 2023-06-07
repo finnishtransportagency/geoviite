fdbd9f14c4bb4e66b04b793ed964d28c72bad714
Jyrki Jakobsson <jyrki.jakobsson@solita.fi>
2023-06-07 12:38:21 +0300

    GVT-1906 remove redundant default values from migration

--- infra/src/main/resources/db/migration/prod/V39__add_element_listing_file_table.sql
+++ infra/src/main/resources/db/migration/prod/V39__add_element_listing_file_table.sql
@@ -3,6 +3,6 @@ create table layout.element_listing_file
   id          int primary key generated always as (1) stored,
   name        varchar(100) not null,
   content     varchar      not null,
-  change_time timestamptz  not null default now(),
-  change_user varchar      not null default current_setting('geoviite.edit_user')
+  change_time timestamptz  not null,
+  change_user varchar(30)  not null
 );
