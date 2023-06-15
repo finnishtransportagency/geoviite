b413f8a4c06d08b76d77c932556873cd790b86b7
Juuse Koivula <juuse.koivula@solita.fi>
2023-06-15 16:58:31 +0300

    GVT-1915: Make InfraModel header fetch happen earlier so that the tab header will always show a document count properly

--- ui/src/infra-model/tabs/infra-model-tabs.tsx
+++ ui/src/infra-model/tabs/infra-model-tabs.tsx
@@ -4,11 +4,13 @@ import InfraModelTabContent from 'infra-model/tabs/infra-model-tab-content';
 import { useCommonDataAppSelector, useInfraModelAppSelector } from 'store/hooks';
 import { InfraModelListContainer } from 'infra-model/list/infra-model-list-container';
 import { useTranslation } from 'react-i18next';
-import { InfraModelTabType } from 'infra-model/infra-model-slice';
+import { infraModelActionCreators, InfraModelTabType } from 'infra-model/infra-model-slice';
 import { PVFileListContainer } from 'infra-model/projektivelho/pv-file-list';
 import styles from 'infra-model/tabs/infra-model-tabs.scss';
 import { useLoader } from 'utils/react-utils';
 import { getPVDocumentCount } from 'infra-model/infra-model-api';
+import { getGeometryPlanHeadersBySearchTerms } from 'geometry/geometry-api';
+import { createDelegates } from 'store/store-utils';
 
 export type TabsProps = {
     activeTab: InfraModelTabType;
@@ -18,10 +20,37 @@ const InfraModelTabs: React.FC<TabsProps> = ({ activeTab }) => {
     const { t } = useTranslation();
     const changeTimes = useCommonDataAppSelector((state) => state.changeTimes);
     const changeTime = useCommonDataAppSelector((state) => state.changeTimes.pvDocument);
+    const infraModelListDelegates = createDelegates(infraModelActionCreators);
+    const state = useInfraModelAppSelector((state) => state.infraModelList);
     const numberOfInfraModelFiles = useInfraModelAppSelector(
         (state) => state.infraModelList.totalCount,
     );
     const documentCounts = useLoader(() => getPVDocumentCount(), [changeTime]);
+
+    // Handle search plans side effect
+    React.useEffect(() => {
+        if (state.searchState == 'start') {
+            infraModelListDelegates.onPlanFetchStart();
+            getGeometryPlanHeadersBySearchTerms(
+                state.pageSize,
+                state.page * state.pageSize,
+                undefined,
+                state.searchParams.sources,
+                state.searchParams.trackNumberIds,
+                state.searchParams.freeText,
+                state.searchParams.sortBy,
+                state.searchParams.sortOrder,
+            )
+                .then((page) => {
+                    infraModelListDelegates.onPlansFetchReady({
+                        plans: page.items,
+                        searchParams: state.searchParams,
+                        totalCount: page.totalCount,
+                    });
+                })
+                .catch((e) => infraModelListDelegates.onPlanFetchError(e?.toString()));
+        }
+    }, [state.searchState]);
 
     return (
         <div className={styles['tabs__tab-container']}>
@@ -51,7 +80,14 @@ const InfraModelTabs: React.FC<TabsProps> = ({ activeTab }) => {
             </ul>
             <div className={styles['tabs__outlet']}>
                 <InfraModelTabContent tabId={InfraModelTabType.PLAN} activeTab={activeTab}>
-                    <InfraModelListContainer changeTimes={changeTimes} />
+                    <InfraModelListContainer
+                        changeTimes={changeTimes}
+                        clearInfraModelState={infraModelListDelegates.clearInfraModelState}
+                        onPlanChangeTimeChange={infraModelListDelegates.onPlanChangeTimeChange}
+                        onSearchParamsChange={infraModelListDelegates.onSearchParamsChange}
+                        onPrevPage={infraModelListDelegates.onPrevPage}
+                        onNextPage={infraModelListDelegates.onNextPage}
+                    />
                 </InfraModelTabContent>
                 <InfraModelTabContent tabId={InfraModelTabType.WAITING} activeTab={activeTab}>
                     <PVFileListContainer listMode={'SUGGESTED'} changeTime={changeTime} />
