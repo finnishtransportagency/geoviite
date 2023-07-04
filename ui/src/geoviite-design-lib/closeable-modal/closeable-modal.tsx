d7ea5fdbf59686c993f4f3e91cbc85eb5ab50b29
Taru Saloheimo <92579564+solita-taru@users.noreply.github.com>
2023-07-04 10:08:08 +0300

    GVT-1981: General closeable component (#581)
    (cherry picked from commit 7356c43fd3775b879ddf8cf551d616e9b4d20419)

---
+++ ui/src/geoviite-design-lib/closeable-modal/closeable-modal.tsx
@@ -0,0 +1,66 @@
+import * as React from 'react';
+import { createPortal } from 'react-dom';
+import useResizeObserver from 'use-resize-observer';
+
+type CloseableModalProps = {
+    positionRef: React.MutableRefObject<HTMLElement | null>;
+    onClickOutside: () => void;
+    offsetX: number;
+    offsetY: number;
+    children: React.ReactNode;
+};
+
+export const CloseableModal: React.FC<CloseableModalProps> = ({
+    positionRef,
+    onClickOutside,
+    offsetX,
+    offsetY,
+    children,
+}: CloseableModalProps) => {
+    const [x, setX] = React.useState<number>();
+    const [y, setY] = React.useState<number>();
+
+    const boundingRect = positionRef.current?.getBoundingClientRect();
+
+    React.useEffect(() => {
+        setX(boundingRect?.x);
+        setY(boundingRect?.y);
+    }, [boundingRect?.x, boundingRect?.y]);
+
+    React.useEffect(() => {
+        function clickHandler(event: MouseEvent) {
+            if (positionRef.current && !positionRef.current.contains(event.target as HTMLElement)) {
+                onClickOutside();
+            }
+        }
+
+        document.addEventListener('click', clickHandler);
+
+        return () => {
+            document.removeEventListener('click', clickHandler);
+        };
+    }, [positionRef]);
+
+    useResizeObserver({
+        ref: document.body,
+        onResize: () => {
+            setX(boundingRect?.x);
+            setY(boundingRect?.y);
+        },
+    });
+
+    if (x === undefined || y === undefined) return <></>;
+
+    return createPortal(
+        <div
+            style={{
+                top: y + offsetY,
+                left: x + offsetX,
+                position: 'absolute',
+            }}
+            onClick={(e) => e.stopPropagation()}>
+            {children}
+        </div>,
+        document.body,
+    );
+};
