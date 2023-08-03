c72172d1938f06bdc179535bb9919934a25c65e3
Taru Saloheimo <taru.saloheimo@solita.fi>
2023-08-03 14:40:44 +0300

    GVT-1297 workflow testing

--- ui/src/main/main.tsx
+++ ui/src/main/main.tsx
@@ -33,7 +33,7 @@ type MainProps = {
     layoutMode: LayoutMode;
     version: string | undefined;
 };
-
+//this is a test
 const Main: React.VFC<MainProps> = (props: MainProps) => {
     const { t } = useTranslation();
     return (
