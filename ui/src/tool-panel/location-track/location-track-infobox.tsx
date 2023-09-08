d5982424fdff1b36f3953ff66cfe010ad52ac66f
Juuse Koivula <juuse.koivula@solita.fi>
2023-09-08 16:00:04 +0300

    GVT-2038: Improve location track location infobox for unlinked location tracks

--- ui/src/tool-panel/location-track/location-track-infobox.tsx
+++ ui/src/tool-panel/location-track/location-track-infobox.tsx
@@ -306,10 +306,18 @@ const LocationTrackInfobox: React.FC<LocationTrackInfoboxProps> = ({
                     qa-id="location-track-location-infobox">
                     <InfoboxContent>
                         <InfoboxField label={t('tool-panel.location-track.start-location')}>
-                            <TrackMeter value={startAndEndPoints?.start?.address} />
+                            {startAndEndPoints?.start?.address ? (
+                                <TrackMeter value={startAndEndPoints?.start?.address} />
+                            ) : (
+                                t('tool-panel.location-track.unset')
+                            )}
                         </InfoboxField>
                         <InfoboxField label={t('tool-panel.location-track.end-location')}>
-                            <TrackMeter value={startAndEndPoints?.end?.address} />
+                            {startAndEndPoints?.end?.address ? (
+                                <TrackMeter value={startAndEndPoints?.end?.address} />
+                            ) : (
+                                t('tool-panel.location-track.unset')
+                            )}
                         </InfoboxField>
 
                         {linkingState === undefined && (
@@ -318,6 +326,10 @@ const LocationTrackInfobox: React.FC<LocationTrackInfoboxProps> = ({
                                     <Button
                                         variant={ButtonVariant.SECONDARY}
                                         size={ButtonSize.SMALL}
+                                        disabled={
+                                            !startAndEndPoints.start?.point ||
+                                            !startAndEndPoints.end?.point
+                                        }
                                         onClick={() => {
                                             getEndLinkPoints(
                                                 locationTrack.id,
@@ -376,7 +388,7 @@ const LocationTrackInfobox: React.FC<LocationTrackInfoboxProps> = ({
                             value={
                                 startAndEndPoints?.start
                                     ? formatToTM35FINString(startAndEndPoints.start.point)
-                                    : ''
+                                    : t('tool-panel.location-track.unset')
                             }
                         />
                         <InfoboxField
@@ -386,7 +398,7 @@ const LocationTrackInfobox: React.FC<LocationTrackInfoboxProps> = ({
                             value={
                                 startAndEndPoints?.end
                                     ? formatToTM35FINString(startAndEndPoints?.end.point)
-                                    : '-'
+                                    : t('tool-panel.location-track.unset')
                             }
                         />
                     </InfoboxContent>
