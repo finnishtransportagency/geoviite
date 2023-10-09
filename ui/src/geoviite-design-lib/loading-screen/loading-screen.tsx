cb9babd22661336fa3d06f59c226db8bfbf35ce4
Tiandu Zhao <tiandu.zhao@solita.fi>
2023-10-09 13:50:27 +0300

    GVT-2087: More descriptive alt text

--- ui/src/geoviite-design-lib/loading-screen/loading-screen.tsx
+++ ui/src/geoviite-design-lib/loading-screen/loading-screen.tsx
@@ -5,7 +5,7 @@ import geoviiteLogo from 'geoviite-design-lib/geoviite-logo.svg';
 export const LoadingScreen: React.FC = () => {
     return (
         <div className={styles['loading-screen']}>
-            <img src={geoviiteLogo} alt="Latausikkuna" />
+            <img src={geoviiteLogo} alt="Geoviitettä ladataan" />
         </div>
     );
 };
