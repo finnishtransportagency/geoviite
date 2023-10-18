8fa7dbc1e574f26e24cb4d90a28836f70d9ef591
Ari Rahikkala <ari.rahikkala@solita.fi>
2023-10-18 13:29:30 +0300

    GVT-2177 Expand and clarify test

--- infra/src/test/kotlin/fi/fta/geoviite/infra/publication/PublicationServiceIT.kt
+++ infra/src/test/kotlin/fi/fta/geoviite/infra/publication/PublicationServiceIT.kt
@@ -1340,38 +1340,61 @@ class PublicationServiceIT @Autowired constructor(
 
     @Test
     fun `Location track switch link changes are reported`() {
-        val switch1 = switchDao.insert(switch(1, name = "sw 1"))
-        val switch2 = switchDao.insert(switch(2, name = "sw 2"))
-        val switch3 = switchDao.insert(switch(3, name = "sw 3"))
-        val switch4 = switchDao.insert(switch(4, name = "sw 4"))
-        val switch5 = switchDao.insert(switch(5, name = "sw 5"))
+        val switchUnlinkedFromTopology = switchDao.insert(switch(1, name = "sw-unlinked-from-topology"))
+        val switchUnlinkedFromAlignment = switchDao.insert(switch(2, name = "sw-unlinked-from-alignment"))
+        val switchAddedToTopologyStart = switchDao.insert(switch(3, name = "sw-added-to-topo-start"))
+        val switchAddedToTopologyEnd = switchDao.insert(switch(4, name = "sw-added-to-topo-end"))
+        val switchAddedToAlignment = switchDao.insert(switch(5, name = "sw-added-to-alignment"))
+        val switchDeleted = switchDao.insert(switch(6, name = "sw-deleted"))
+        val switchMerelyRenamed = switchDao.insert(switch(7, name = "sw-merely-renamed"))
 
         val trackNumberId = getUnusedTrackNumberId()
         val originalLocationTrack = locationTrackService.saveDraft(
-            locationTrack(trackNumberId, topologyStartSwitch = TopologyLocationTrackSwitch(switch1.id, JointNumber(1))),
+            locationTrack(trackNumberId, topologyStartSwitch = TopologyLocationTrackSwitch(switchUnlinkedFromTopology.id, JointNumber(1))),
             alignment(
                 segment(Point(0.0, 0.0), Point(0.0, 1.0)).copy(
-                    switchId = switch2.id,
+                    switchId = switchUnlinkedFromAlignment.id,
                     startJointNumber = JointNumber(1)
                 ),
-                segment(Point(0.0, 1.0), Point(0.0, 2.0))
+                segment(Point(0.0, 1.0), Point(0.0, 2.0)).copy(
+                    switchId = switchDeleted.id,
+                    startJointNumber = JointNumber(1)
+                ),
+                segment(Point(0.0, 2.0), Point(0.0, 3.0)).copy(
+                    switchId = switchMerelyRenamed.id,
+                    startJointNumber = JointNumber(7)
+                ),
             )
         )
         publish(publicationService, locationTracks = listOf(originalLocationTrack.id))
+        switchService.saveDraft(
+            switchDao.fetch(switchDeleted.rowVersion).copy(stateCategory = LayoutStateCategory.NOT_EXISTING)
+        )
+        switchService.saveDraft(
+            switchDao.fetch(switchMerelyRenamed.rowVersion).copy(name = SwitchName("sw-with-new-name"))
+        )
         locationTrackService.saveDraft(
             locationTrackDao.fetch(locationTrackDao.fetchVersion(originalLocationTrack.id, OFFICIAL)!!).copy(
-                topologyStartSwitch = TopologyLocationTrackSwitch(switch3.id, JointNumber(1)),
-                topologyEndSwitch = TopologyLocationTrackSwitch(switch4.id, JointNumber(1))
+                topologyStartSwitch = TopologyLocationTrackSwitch(switchAddedToTopologyStart.id, JointNumber(1)),
+                topologyEndSwitch = TopologyLocationTrackSwitch(switchAddedToTopologyEnd.id, JointNumber(1))
             ),
             alignment(
                 segment(Point(0.0, 0.0), Point(0.0, 1.0)),
                 segment(Point(0.0, 1.0), Point(0.0, 2.0)).copy(
-                    switchId = switch5.id,
+                    switchId = switchAddedToAlignment.id,
+                    startJointNumber = JointNumber(1)
+                ),
+                segment(Point(0.0, 2.0), Point(0.0, 3.0)).copy(
+                    switchId = switchMerelyRenamed.id,
                     startJointNumber = JointNumber(1)
                 ),
             )
         )
-        publish(publicationService, locationTracks = listOf(originalLocationTrack.id))
+        publish(
+            publicationService,
+            locationTracks = listOf(originalLocationTrack.id),
+            switches = listOf(switchDeleted.id, switchMerelyRenamed.id)
+        )
         val latestPubs = publicationService.fetchLatestPublicationDetails(2)
         val latestPub = latestPubs[0]
         val previousPub = latestPubs[1]
@@ -1388,8 +1411,9 @@ class PublicationServiceIT @Autowired constructor(
         ) { _, _ -> null }
         assertEquals(1, diff.size)
         assertEquals("linked-switches", diff[0].propKey.key.toString())
-        assertEquals("Vaihteiden sw 1, sw 2 linkitys purettu. Vaihteet sw 3, sw 4, sw 5 linkitetty.", diff[0].remark)
+        assertEquals("Vaihteiden sw-deleted, sw-unlinked-from-alignment, sw-unlinked-from-topology linkitys purettu. Vaihteet sw-added-to-alignment, sw-added-to-topo-end, sw-added-to-topo-start linkitetty.", diff[0].remark)
     }
+
 
     @Test
     fun `should filter publication details by dates`() {
