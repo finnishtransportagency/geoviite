21f743f609ef4d6e5963d59d967b50e11de98c25
Taru Saloheimo <taru.saloheimo@solita.fi>
2023-11-16 10:40:36 +0200

    GVT-2040 Test how failing test is shown in Junit reporter

--- infra/src/test/kotlin/fi/fta/geoviite/infra/geometry/CoordinateTransformServiceIT.kt
+++ infra/src/test/kotlin/fi/fta/geoviite/infra/geometry/CoordinateTransformServiceIT.kt
@@ -147,6 +147,6 @@ class CoordinateTransformServiceIT @Autowired constructor(
         val transformedPoint = coordinateTransformationService.transformCoordinate(LAYOUT_SRID, KKJ5, point)
         // Expected values are from paikkatietoikkuna
         Assertions.assertEquals(5426728.7305, transformedPoint.x, 0.001)
-        Assertions.assertEquals(6978302.5687, transformedPoint.y, 0.001)
+        Assertions.assertEquals(6978302.5687, transformedPoint.x, 0.001)
     }
 }
