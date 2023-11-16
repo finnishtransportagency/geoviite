8620e575cd79cc5c886e3fcc7839fcf44095f3e0
Jyrki Jakobsson <jyrki.jakobsson@solita.fi>
2023-11-16 18:42:32 +0200

    GVT-2256 Project creation does not need to return the same object

--- ui/src/infra-model/view/dialogs/new-project-dialog.tsx
+++ ui/src/infra-model/view/dialogs/new-project-dialog.tsx
@@ -7,7 +7,7 @@ import { FormLayoutColumn } from 'geoviite-design-lib/form-layout/form-layout';
 import { FieldLayout } from 'vayla-design-lib/field-layout/field-layout';
 import { TextField } from 'vayla-design-lib/text-field/text-field';
 import { useTranslation } from 'react-i18next';
-import { Project } from 'geometry/geometry-model';
+import { Project, ProjectId } from 'geometry/geometry-model';
 import { debounce } from 'ts-debounce';
 import { createProject, getProjects } from 'geometry/geometry-api';
 import * as Snackbar from 'geoviite-design-lib/snackbar/snackbar';
@@ -16,7 +16,7 @@ import { useLoader } from 'utils/react-utils';
 
 type NewProjectDialogProps = {
     onClose: () => void;
-    onSave: (project: Project) => void;
+    onSave: (projectId: ProjectId) => void;
 };
 
 export const NewProjectDialog: React.FC<NewProjectDialogProps> = ({ onClose, onSave }) => {
@@ -54,13 +54,13 @@ export const NewProjectDialog: React.FC<NewProjectDialogProps> = ({ onClose, onS
 
     const saveProject = () => {
         setSaveInProgress(true);
-        createProject({ name: projectName } as Project).then((p) => {
+        createProject({ name: projectName } as Project).then((id) => {
             setCanSave(false);
             setSaveInProgress(false);
 
             if (p) {
                 Snackbar.success('im-form.new-project-created');
-                onSave(p);
+                onSave(id);
             } else {
                 Snackbar.error('im-form.new-project-creation-failed');
             }
