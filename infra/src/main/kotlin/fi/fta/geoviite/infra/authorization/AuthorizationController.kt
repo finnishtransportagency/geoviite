bb0de567160ee9291bed95c7b808dd6655204191
solita-mrunsten <128050934+solita-mrunsten@users.noreply.github.com>
2023-11-30 18:41:52 +0200

    GVT-2257: Test cached postgres image from main

--- infra/src/main/kotlin/fi/fta/geoviite/infra/authorization/AuthorizationController.kt
+++ infra/src/main/kotlin/fi/fta/geoviite/infra/authorization/AuthorizationController.kt
@@ -14,6 +14,7 @@ import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.bind.annotation.*
 
+//
 @RestController
 @RequestMapping("/authorization")
 class AuthorizationController @Autowired constructor(private val signer: CookieSigner) {
