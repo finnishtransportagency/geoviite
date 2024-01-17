b20b84a34ccc0b86a38d586ac9012439bda62e04
Jyrki Jakobsson <jyrki.jakobsson@solita.fi>
2024-01-17 16:18:06 +0200

    GVT-2154 Add controller annotations

--- infra/src/main/kotlin/fi/fta/geoviite/infra/split/SplitController.kt
+++ infra/src/main/kotlin/fi/fta/geoviite/infra/split/SplitController.kt
@@ -1,11 +1,19 @@
 package fi.fta.geoviite.infra.split
 
+import fi.fta.geoviite.infra.authorization.AUTH_ALL_WRITE
 import fi.fta.geoviite.infra.common.IntId
-import org.springframework.stereotype.Controller
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
-@Controller
+@RestController
+@RequestMapping("/location-track-split")
 class SplitController(val splitService: SplitService) {
-    fun splitLocationTrack(request: SplitRequest): IntId<SplitSource> {
+    @PreAuthorize(AUTH_ALL_WRITE)
+    @PostMapping
+    fun splitLocationTrack(@RequestBody request: SplitRequest): IntId<SplitSource> {
         return splitService.split(request)
     }
 }
