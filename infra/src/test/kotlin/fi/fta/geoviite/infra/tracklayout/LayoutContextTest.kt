9b8553af736e2519ca3a31568966f8bbf89c5b8c
Jyrki Jakobsson <jyrki.jakobsson@solita.fi>
2024-03-08 14:51:40 +0200

    GVT-2426 Unit tests (+placeholders) for context data logic

---
+++ infra/src/test/kotlin/fi/fta/geoviite/infra/tracklayout/LayoutContextTest.kt
@@ -0,0 +1,153 @@
+package fi.fta.geoviite.infra.tracklayout
+
+import fi.fta.geoviite.infra.common.DataType.TEMP
+import fi.fta.geoviite.infra.common.IntId
+import fi.fta.geoviite.infra.common.StringId
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class LayoutContextTest {
+
+    @Test
+    fun `Context ID is resolved correctly`() {
+        val rowId = StringId<TrackLayoutTrackNumber>()
+        val officialRowId = StringId<TrackLayoutTrackNumber>()
+        val designRowId = StringId<TrackLayoutTrackNumber>()
+
+        assertEquals(rowId, MainOfficialContextData(rowId, TEMP).id)
+
+        assertEquals(officialRowId, MainDraftContextData(rowId, officialRowId, designRowId, TEMP).id)
+        assertEquals(designRowId, MainDraftContextData(rowId, null, designRowId, TEMP).id)
+        assertEquals(rowId, MainDraftContextData(rowId, null, null, TEMP).id)
+
+        assertEquals(officialRowId, DesignOfficialContextData(rowId, officialRowId, IntId(1), TEMP).id)
+        assertEquals(rowId, DesignOfficialContextData(rowId, null, IntId(1), TEMP).id)
+
+        assertEquals(officialRowId, DesignDraftContextData(rowId, designRowId, officialRowId, IntId(1), TEMP).id)
+        assertEquals(designRowId, DesignDraftContextData(rowId, designRowId, null, IntId(1), TEMP).id)
+        assertEquals(rowId, DesignDraftContextData(rowId, null, null, IntId(1), TEMP).id)
+    }
+
+    @Test
+    fun `Official context calculated fields work as expected`() {
+        val id = StringId<TrackLayoutTrackNumber>()
+        val context = MainOfficialContextData(id, TEMP)
+        assertEquals(ContextType.OFFICIAL, context.draftType)
+        assertFalse(context.isDesign)
+        assertTrue(context.isOfficial)
+        assertFalse(context.isDraft)
+        assertFalse(context.isEditedDraft)
+        assertFalse(context.isNewDraft)
+    }
+
+    @Test
+    fun `Draft context calculated fields work as expected`() {
+        val id = StringId<TrackLayoutTrackNumber>()
+        val officialRowId = StringId<TrackLayoutTrackNumber>()
+        val designRowId = StringId<TrackLayoutTrackNumber>()
+
+        val editedContext = MainDraftContextData(id, officialRowId, designRowId, TEMP)
+        assertEquals(ContextType.EDITED_DRAFT, editedContext.draftType)
+        assertFalse(editedContext.isDesign)
+        assertFalse(editedContext.isOfficial)
+        assertTrue(editedContext.isDraft)
+        assertTrue(editedContext.isEditedDraft)
+        assertFalse(editedContext.isNewDraft)
+
+        val newContext = MainDraftContextData(id, null, designRowId, TEMP)
+        assertEquals(ContextType.NEW_DRAFT, newContext.draftType)
+        assertFalse(newContext.isDesign)
+        assertFalse(newContext.isOfficial)
+        assertTrue(newContext.isDraft)
+        assertFalse(newContext.isEditedDraft)
+        assertTrue(newContext.isNewDraft)
+
+        val newContext2 = MainDraftContextData(id, null, null, TEMP)
+        assertEquals(ContextType.NEW_DRAFT, newContext2.draftType)
+        assertFalse(newContext2.isDesign)
+        assertFalse(newContext2.isOfficial)
+        assertTrue(newContext2.isDraft)
+        assertFalse(newContext2.isEditedDraft)
+        assertTrue(newContext2.isNewDraft)
+    }
+
+    @Test
+    fun `Official design context calculated fields work as expected`() {
+        val id = StringId<TrackLayoutTrackNumber>()
+        val officialRowId = StringId<TrackLayoutTrackNumber>()
+
+        val context = DesignOfficialContextData(id, officialRowId, IntId(1), TEMP)
+        assertEquals(ContextType.DESIGN, context.draftType)
+        assertTrue(context.isDesign)
+        assertFalse(context.isOfficial)
+        assertFalse(context.isDraft)
+        assertFalse(context.isEditedDraft)
+        assertFalse(context.isNewDraft)
+
+        val newContext = DesignOfficialContextData(id, null, IntId(1), TEMP)
+        assertEquals(ContextType.DESIGN, newContext.draftType)
+        assertTrue(newContext.isDesign)
+        assertFalse(newContext.isOfficial)
+        assertFalse(newContext.isDraft)
+        assertFalse(newContext.isEditedDraft)
+        assertFalse(newContext.isNewDraft)
+    }
+
+    @Test
+    fun `Draft design context calculated fields work as expected`() {
+        val id = StringId<TrackLayoutTrackNumber>()
+        val officialRowId = StringId<TrackLayoutTrackNumber>()
+        val designRowId = StringId<TrackLayoutTrackNumber>()
+
+        val context = DesignDraftContextData(id, designRowId, officialRowId, IntId(1), TEMP)
+        assertEquals(ContextType.EDITED_DESIGN_DRAFT, context.draftType)
+        assertTrue(context.isDesign)
+        assertFalse(context.isOfficial)
+        assertTrue(context.isDraft)
+        assertTrue(context.isEditedDraft)
+        assertFalse(context.isNewDraft)
+
+        val newContext = DesignDraftContextData(id, null, officialRowId, IntId(1), TEMP)
+        assertEquals(ContextType.NEW_DESIGN_DRAFT, newContext.draftType)
+        assertTrue(newContext.isDesign)
+        assertFalse(newContext.isOfficial)
+        assertTrue(newContext.isDraft)
+        assertFalse(newContext.isEditedDraft)
+        assertTrue(newContext.isNewDraft)
+
+        val newContext2 = DesignDraftContextData(id, null, null, IntId(1), TEMP)
+        assertEquals(ContextType.NEW_DESIGN_DRAFT, newContext2.draftType)
+        assertTrue(newContext2.isDesign)
+        assertFalse(newContext2.isOfficial)
+        assertTrue(newContext2.isDraft)
+        assertFalse(newContext2.isEditedDraft)
+        assertTrue(newContext2.isNewDraft)
+    }
+
+    @Test
+    fun `Draft is correctly created from Official`() {
+        TODO("Not tested yet")
+    }
+
+    @Test
+    fun `Official is correctly created from Draft`() {
+        TODO("Not tested yet")
+    }
+
+    @Test
+    fun `Design-Draft is correctly created from Official`() {
+        TODO("Not tested yet")
+    }
+
+    @Test
+    fun `Design-Draft is correctly created from Design`() {
+        TODO("Not tested yet")
+    }
+
+    @Test
+    fun `Design is correctly created from Design-Draft`() {
+        TODO("Not tested yet")
+    }
+}
