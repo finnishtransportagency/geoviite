6d6a005539b395c90f54c293a54cc14361ff599d
Juuse Koivula <juuse.koivula@solita.fi>
2024-03-18 15:49:39 +0200

    GVT-2459: Select track number fetch publishType based on privileges in inframodel views. Also rename publicationState to publishType to make sure SpEL mappings work properly

--- ui/src/infra-model/list/infra-model-search-result.tsx
+++ ui/src/infra-model/list/infra-model-search-result.tsx
@@ -23,7 +23,13 @@ import { GeometryPlanLinkingSummary, getGeometryPlanLinkingSummaries } from 'geo
 import { ConfirmHideInfraModel } from './confirm-hide-infra-model-dialog';
 import { ConfirmDownloadUnreliableInfraModelDialog } from './confirm-download-unreliable-infra-model-dialog';
 import { PrivilegeRequired } from 'user/privilege-required';
-import { DOWNLOAD_GEOMETRY, EDIT_GEOMETRY_FILE } from 'user/user-model';
+import {
+    DOWNLOAD_GEOMETRY,
+    EDIT_GEOMETRY_FILE,
+    userHasPrivilege,
+    VIEW_LAYOUT_DRAFT,
+} from 'user/user-model';
+import { useCommonDataAppSelector } from 'store/hooks';
 
 export type InfraModelSearchResultProps = Pick<
     InfraModelListState,
@@ -63,7 +69,10 @@ function toggleSortOrder(
 export const InfraModelSearchResult: React.FC<InfraModelSearchResultProps> = (
     props: InfraModelSearchResultProps,
 ) => {
-    const trackNumbers = useTrackNumbers('OFFICIAL');
+    const privileges = useCommonDataAppSelector((state) => state.userPrivileges).map((p) => p.code);
+    const trackNumbers = useTrackNumbers(
+        userHasPrivilege(privileges, VIEW_LAYOUT_DRAFT) ? 'DRAFT' : 'OFFICIAL',
+    );
 
     const [linkingSummaries, setLinkingSummaries] = useState<
         Map<GeometryPlanId, GeometryPlanLinkingSummary | undefined>
