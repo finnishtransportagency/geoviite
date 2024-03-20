254b5c03894339d86e3761718fef99f25957c953
Ari Rahikkala <ari.rahikkala@solita.fi>
2024-03-20 12:20:10 +0200

    GVT-2469 Actually pass changeTimes

--- ui/src/track-layout/layout-operating-point-api.ts
+++ ui/src/track-layout/layout-operating-point-api.ts
@@ -3,11 +3,15 @@ import { getNonNull, queryParams } from 'api/api-fetch';
 import { asyncCache } from 'cache/cache';
 import { MapTile } from 'map/map-model';
 import { bboxString } from 'common/common-api';
+import { TimeStamp } from 'common/common-model';
 
 const operatingPointCache = asyncCache<string, OperatingPoint[]>();
 
-export async function getOperatingPoints(mapTile: MapTile): Promise<OperatingPoint[]> {
-    return operatingPointCache.get('2000-01-01 00:00:00', mapTile.id, () => {
+export async function getOperatingPoints(
+    mapTile: MapTile,
+    changeTime: TimeStamp,
+): Promise<OperatingPoint[]> {
+    return operatingPointCache.get(changeTime, mapTile.id, () => {
         const params = queryParams({ boundingBox: bboxString(mapTile.area) });
         return getNonNull<OperatingPoint[]>(`api/ratko/operating-points${params}`);
     });
