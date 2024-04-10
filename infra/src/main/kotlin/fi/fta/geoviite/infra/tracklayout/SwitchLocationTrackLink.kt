ba2ccead50de677cb3253811791b5a5f20efbd72
Jyrki Jakobsson <jyrki.jakobsson@solita.fi>
2024-04-10 18:21:19 +0300

    GVT-2525 remove redundant code

--- infra/src/main/kotlin/fi/fta/geoviite/infra/tracklayout/SwitchLocationTrackLink.kt
+++ infra/src/main/kotlin/fi/fta/geoviite/infra/tracklayout/SwitchLocationTrackLink.kt
@@ -161,36 +161,3 @@ fun getSwitchJoints(segment: LayoutSegment): List<Pair<IntId<TrackLayoutSwitch>,
     segment.switchId
         ?.let { id -> listOfNotNull(segment.startJointNumber, segment.endJointNumber).map { id as IntId to it } }
         ?: emptyList()
-
-data class SwitchJointLocation(
-    val switchId: IntId<TrackLayoutSwitch>,
-    val jointNumber: JointNumber,
-    val location: Point,
-)
-
-fun collectSwitchJointLocations(track: LocationTrack, alignment: LayoutAlignment): List<SwitchJointLocation> {
-    val allJoints = listOf(
-        listOfNotNull(getSwitchJointLocation(track.topologyStartSwitch, alignment.start)),
-        alignment.segments.flatMap(::getSwitchJointLocations),
-        listOfNotNull(getSwitchJointLocation(track.topologyEndSwitch, alignment.end)),
-    ).flatten().distinctBy { sjl -> sjl.switchId to sjl.jointNumber }
-    // Skip all but first/last joint of each switch
-    return allJoints.filterIndexed { index, (id, _) ->
-        allJoints.getOrNull(index - 1)?.switchId != id || allJoints.getOrNull(index + 1)?.switchId != id
-    }
-}
-
-private fun getSwitchJointLocation(
-    topologySwitch: TopologyLocationTrackSwitch?,
-    location: IPoint?,
-): SwitchJointLocation? =
-    topologySwitch?.let { s -> location?.let { l -> SwitchJointLocation(s.switchId, s.jointNumber, l.toPoint()) } }
-
-fun getSwitchJointLocations(segment: LayoutSegment): List<SwitchJointLocation> =
-    segment.switchId
-        ?.let { id ->
-            listOfNotNull(
-                segment.startJointNumber?.let { j -> segment.segmentStart to j },
-                segment.endJointNumber?.let { j -> segment.segmentEnd to j },
-            ).map { (point, joint) -> SwitchJointLocation(id as IntId, joint, point.toPoint()) }
-        } ?: emptyList()
