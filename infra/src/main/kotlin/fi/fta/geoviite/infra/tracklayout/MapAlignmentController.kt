0cd6c5146b7d7c5229e738f7c5e370a351e8fea2
Jyrki Jakobsson <jyrki.jakobsson@solita.fi>
2024-05-08 10:03:21 +0300

    General: Clean up detekt warnings: there's no need to string-template a variable that is a string

--- infra/src/main/kotlin/fi/fta/geoviite/infra/tracklayout/MapAlignmentController.kt
+++ infra/src/main/kotlin/fi/fta/geoviite/infra/tracklayout/MapAlignmentController.kt
@@ -12,7 +12,11 @@ import fi.fta.geoviite.infra.tracklayout.AlignmentFetchType.ALL
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.security.access.prepost.PreAuthorize
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/track-layout/map")
@@ -22,14 +26,14 @@ class MapAlignmentController(private val mapAlignmentService: MapAlignmentServic
     @PreAuthorize(AUTH_VIEW_DRAFT_OR_OFFICIAL_BY_PUBLICATION_STATE)
     @GetMapping("/{$PUBLICATION_STATE}/alignment-polylines")
     fun getAlignmentPolyLines(
-        @PathVariable("$PUBLICATION_STATE") publicationState: PublicationState,
+        @PathVariable(PUBLICATION_STATE) publicationState: PublicationState,
         @RequestParam("bbox") bbox: BoundingBox,
         @RequestParam("resolution") resolution: Int,
         @RequestParam("type") type: AlignmentFetchType? = null,
     ): List<AlignmentPolyLine<*>> {
         logger.apiCall(
             "getAlignmentPolyLines",
-            "$PUBLICATION_STATE" to publicationState,
+            PUBLICATION_STATE to publicationState,
             "bbox" to bbox,
             "resolution" to resolution,
             "type" to type,
@@ -40,14 +44,14 @@ class MapAlignmentController(private val mapAlignmentService: MapAlignmentServic
     @PreAuthorize(AUTH_VIEW_DRAFT_OR_OFFICIAL_BY_PUBLICATION_STATE)
     @GetMapping("/{$PUBLICATION_STATE}/location-track/{id}/alignment-polyline")
     fun getLocationTrackPolyline(
-        @PathVariable("$PUBLICATION_STATE") publicationState: PublicationState,
+        @PathVariable(PUBLICATION_STATE) publicationState: PublicationState,
         @PathVariable("id") locationTrackId: IntId<LocationTrack>,
         @RequestParam("bbox") bbox: BoundingBox,
         @RequestParam("resolution") resolution: Int,
     ): AlignmentPolyLine<LocationTrack>? {
         logger.apiCall(
             "getLocationTrackPolyline",
-            "$PUBLICATION_STATE" to publicationState,
+            PUBLICATION_STATE to publicationState,
             "id" to locationTrackId,
             "bbox" to bbox,
             "resolution" to resolution
@@ -59,54 +63,54 @@ class MapAlignmentController(private val mapAlignmentService: MapAlignmentServic
     @PreAuthorize(AUTH_VIEW_DRAFT_OR_OFFICIAL_BY_PUBLICATION_STATE)
     @GetMapping("/{$PUBLICATION_STATE}/location-track/alignment-headers")
     fun getLocationTrackHeaders(
-        @PathVariable("$PUBLICATION_STATE") publicationState: PublicationState,
+        @PathVariable(PUBLICATION_STATE) publicationState: PublicationState,
         @RequestParam("ids") ids: List<IntId<LocationTrack>>,
     ): List<AlignmentHeader<LocationTrack, LocationTrackState>> {
-        logger.apiCall("getReferenceLineHeaders", "$PUBLICATION_STATE" to publicationState, "ids" to ids)
+        logger.apiCall("getReferenceLineHeaders", PUBLICATION_STATE to publicationState, "ids" to ids)
         return mapAlignmentService.getLocationTrackHeaders(publicationState, ids)
     }
 
     @PreAuthorize(AUTH_VIEW_DRAFT_OR_OFFICIAL_BY_PUBLICATION_STATE)
     @GetMapping("/{$PUBLICATION_STATE}/reference-line/alignment-headers")
     fun getReferenceLineHeaders(
-        @PathVariable("$PUBLICATION_STATE") publicationState: PublicationState,
+        @PathVariable(PUBLICATION_STATE) publicationState: PublicationState,
         @RequestParam("ids") ids: List<IntId<ReferenceLine>>,
     ): List<AlignmentHeader<ReferenceLine, LayoutState>> {
-        logger.apiCall("getReferenceLineHeaders", "$PUBLICATION_STATE" to publicationState, "ids" to ids)
+        logger.apiCall("getReferenceLineHeaders", PUBLICATION_STATE to publicationState, "ids" to ids)
         return mapAlignmentService.getReferenceLineHeaders(publicationState, ids)
     }
 
     @PreAuthorize(AUTH_VIEW_DRAFT_OR_OFFICIAL_BY_PUBLICATION_STATE)
     @GetMapping("/{$PUBLICATION_STATE}/location-track/{id}/segment-m")
     fun getLocationTrackSegmentMValues(
-        @PathVariable("$PUBLICATION_STATE") publicationState: PublicationState,
+        @PathVariable(PUBLICATION_STATE) publicationState: PublicationState,
         @PathVariable("id") id: IntId<LocationTrack>,
     ): List<Double> {
-        logger.apiCall("getLocationTrackSegmentMValues", "$PUBLICATION_STATE" to publicationState, "id" to id)
+        logger.apiCall("getLocationTrackSegmentMValues", PUBLICATION_STATE to publicationState, "id" to id)
         return mapAlignmentService.getLocationTrackSegmentMValues(publicationState, id)
     }
 
     @PreAuthorize(AUTH_VIEW_DRAFT_OR_OFFICIAL_BY_PUBLICATION_STATE)
     @GetMapping("/{$PUBLICATION_STATE}/alignment/without-linking")
     fun getSectionsWithoutLinking(
-        @PathVariable("$PUBLICATION_STATE") publicationState: PublicationState,
+        @PathVariable(PUBLICATION_STATE) publicationState: PublicationState,
         @RequestParam("bbox") bbox: BoundingBox,
         @RequestParam("type") type: AlignmentFetchType,
     ): List<MapAlignmentHighlight<*>> {
         logger.apiCall("getSectionsWithoutLinking",
-            "$PUBLICATION_STATE" to publicationState, "bbox" to bbox, "type" to type)
+            PUBLICATION_STATE to publicationState, "bbox" to bbox, "type" to type)
         return mapAlignmentService.getSectionsWithoutLinking(publicationState, bbox, type)
     }
 
     @PreAuthorize(AUTH_VIEW_DRAFT_OR_OFFICIAL_BY_PUBLICATION_STATE)
     @GetMapping("/{$PUBLICATION_STATE}/location-track/without-profile")
     fun getSectionsWithoutProfile(
-        @PathVariable("$PUBLICATION_STATE") publicationState: PublicationState,
+        @PathVariable(PUBLICATION_STATE) publicationState: PublicationState,
         @RequestParam("bbox") bbox: BoundingBox,
     ): List<MapAlignmentHighlight<LocationTrack>> {
         logger.apiCall(
             "getSectionsWithoutProfile",
-            "$PUBLICATION_STATE" to publicationState,
+            PUBLICATION_STATE to publicationState,
             "bbox" to bbox,
         )
         return mapAlignmentService.getSectionsWithoutProfile(publicationState, bbox)
@@ -115,30 +119,30 @@ class MapAlignmentController(private val mapAlignmentService: MapAlignmentServic
     @PreAuthorize(AUTH_VIEW_DRAFT_OR_OFFICIAL_BY_PUBLICATION_STATE)
     @GetMapping("/{$PUBLICATION_STATE}/reference-line/{id}/segment-m")
     fun getReferenceLineSegmentMValues(
-        @PathVariable("$PUBLICATION_STATE") publicationState: PublicationState,
+        @PathVariable(PUBLICATION_STATE) publicationState: PublicationState,
         @PathVariable("id") id: IntId<ReferenceLine>,
     ): List<Double> {
-        logger.apiCall("getReferenceLineSegmentMValues", "$PUBLICATION_STATE" to publicationState, "id" to id)
+        logger.apiCall("getReferenceLineSegmentMValues", PUBLICATION_STATE to publicationState, "id" to id)
         return mapAlignmentService.getReferenceLineSegmentMValues(publicationState, id)
     }
 
     @PreAuthorize(AUTH_VIEW_DRAFT_OR_OFFICIAL_BY_PUBLICATION_STATE)
     @GetMapping("/{$PUBLICATION_STATE}/location-track/{id}/ends")
     fun getLocationTrackEnds(
-        @PathVariable("$PUBLICATION_STATE") publicationState: PublicationState,
+        @PathVariable(PUBLICATION_STATE) publicationState: PublicationState,
         @PathVariable("id") id: IntId<LocationTrack>,
     ): MapAlignmentEndPoints {
-        logger.apiCall("getLocationTrackEnds", "$PUBLICATION_STATE" to publicationState, "id" to id)
+        logger.apiCall("getLocationTrackEnds", PUBLICATION_STATE to publicationState, "id" to id)
         return mapAlignmentService.getLocationTrackEnds(publicationState, id)
     }
 
     @PreAuthorize(AUTH_VIEW_DRAFT_OR_OFFICIAL_BY_PUBLICATION_STATE)
     @GetMapping("/{$PUBLICATION_STATE}/reference-line/{id}/ends")
     fun getReferenceLineEnds(
-        @PathVariable("$PUBLICATION_STATE") publicationState: PublicationState,
+        @PathVariable(PUBLICATION_STATE) publicationState: PublicationState,
         @PathVariable("id") id: IntId<ReferenceLine>,
     ): MapAlignmentEndPoints {
-        logger.apiCall("getReferenceLineEnds", "$PUBLICATION_STATE" to publicationState, "id" to id)
+        logger.apiCall("getReferenceLineEnds", PUBLICATION_STATE to publicationState, "id" to id)
         return mapAlignmentService.getReferenceLineEnds(publicationState, id)
     }
 }
