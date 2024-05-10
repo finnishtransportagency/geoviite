6cfa4d0c9875cd1a027954a1161f674fb671af20
Ari Rahikkala <ari.rahikkala@solita.fi>
2024-05-10 14:07:15 +0300

    GVT-2602 Collect more detailed statistics on layout.segment_version

---
+++ infra/src/main/resources/db/migration/prod/V77__add_layout_segment_version_statistics.sql
@@ -0,0 +1,3 @@
+create statistics layout.segment_version_alignment_stats on alignment_id, alignment_version from layout.segment_version;
+alter statistics layout.segment_version_alignment_stats set statistics 1000;
+analyze layout.segment_version;
