281a8d7ae34a7fdf5a51c329aac14e8168f14cd1
solita-mrunsten <128050934+solita-mrunsten@users.noreply.github.com>
2024-07-23 14:05:31 +0300

    General: Fix another invalid E2E selector and style
    This was caused by modifying the TabHeader component to use anchor
    instead of a button, as anchor allowed a button element as its child,
    while button cannot have a button as its child.
    
    Additionally the tab-header styling in the toolpanel was fixed
    to be inline (as previously).

--- infra/src/test/kotlin/fi/fta/geoviite/infra/ui/pagemodel/map/ToolPanel.kt
+++ infra/src/test/kotlin/fi/fta/geoviite/infra/ui/pagemodel/map/ToolPanel.kt
@@ -10,7 +10,7 @@ class E2EToolPanel(parentView: E2EViewFragment) : E2EViewFragment(parentView, By
     fun selectToolPanelTab(tabName: String) {
         logger.info("Select tab $tabName")
 
-        clickChild(By.xpath("//div[@qa-id='tool-panel-tabs']/button[text() = '$tabName']"))
+        clickChild(By.xpath("//div[@qa-id='tool-panel-tabs']/a[text() = '$tabName']"))
     }
 
     val layoutKmPostGeneral: E2ELayoutKmPostGeneralInfoBox by lazy {
