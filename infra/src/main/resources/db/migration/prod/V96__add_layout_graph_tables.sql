f015a8cd1c58ada62a7d78eeee9fc13d0944a40f
Jyrki Jakobsson <jyrki.jakobsson@solita.fi>
2024-11-12 16:50:02 +0200

    GVT-1727 WIP: Tablestructure with global immutable nodes/edges + track-specific edges w. geometry. Conversion partial (track-specifics WIP)

--- infra/src/main/resources/db/migration/prod/V96__add_layout_graph_tables.sql
+++ infra/src/main/resources/db/migration/prod/V96__add_layout_graph_tables.sql
@@ -9,35 +9,84 @@
 -- drop table if exists layout.location_track_node_switch_version;
 -- drop table if exists layout.location_track_node_version;
 -- drop table if exists layout.location_track_node;
+create or replace function layout.calculate_node_key(
+  switch_links int[],
+  start_track int,
+  end_track int
+) returns uuid language sql as $$
+  select
+    case
+      when switch_links is not null then md5(row (switch_links, null, null)::text)::uuid
+      when start_track is not null then md5(row (null, start_track, null)::text)::uuid
+      when end_track is not null then md5(row (null, null, end_track)::text)::uuid
+    end
+$$ immutable;
 
-create table layout.location_track_node
+create table layout.node
 (
   id       int primary key generated always as identity,
-  location postgis.geometry(point, 3067) not null
+  key      uuid not null unique
 );
-select common.add_table_metadata('layout', 'location_track_node');
--- alter table layout.location_track_node add constraint location_track_node_id_version_unique unique (id, version);
-comment on table layout.location_track_node is 'Location track node: a connecting point for switches and other tracks within a location track';
+comment on table layout.node is 'Layout node: a connecting point in the layout graph. Immutable and un-versioned, this is really just an identity with no data.';
+
+-- TODO: Do we want to normalize these as data as well? Really we're just using them for identity
+create table layout.node_switch_joint
+(
+  node_id      int not null references layout.node (id),
+  -- TODO: could reference the official ID table, but not the current one since draft-only rows are deleted permanently
+  switch_id int not null,
+  switch_joint int not null, -- cannot reference joint-table as a current version of the switch might not exist
+  constraint node_joint_unique unique (node_id, switch_id, switch_joint)
+);
+
+-- TODO: Do we want to normalize these as data as well? Really we're just using them for identity
+create table layout.node_track_end
+(
+  node_id      int not null references layout.node (id),
+  -- TODO: could reference the official ID table, but not the current one since draft-only rows are deleted permanently
+  starting_location_track_id int null unique,
+  ending_location_track_id int null unique
+);
+
+-- TODO: Does this need a surrogate key? Do we actually even need this table at all?
+create table layout.edge
+(
+  id       int primary key generated always as identity,
+  start_node int not null references layout.node,
+  end_node int not null references layout.node,
+  constraint edge_nodes_unique unique (start_node, end_node)
+);
+comment on table layout.edge is 'Location track edge: a connection between two nodes of the layout graph. Immutable and un-versioned, this is really just an identity with no data.';
+
+-- create table layout.location_track_node
+-- (
+--   id       int primary key generated always as identity,
+--   location postgis.geometry(point, 3067) not null
+-- );
+-- select common.add_table_metadata('layout', 'location_track_node');
+-- -- alter table layout.location_track_node add constraint location_track_node_id_version_unique unique (id, version);
+-- comment on table layout.location_track_node is 'Location track node: a connecting point for switches and other tracks within a location track';
 
 -- This is a direct version-table under node versioning
 -- Hence it cannot reference main tables directly as rows in those can be deleted
-create table layout.location_track_node_switch_version
-(
-  node_id      int not null,
-  node_version int not null,
-  -- TODO: We could make this a versioned link that does not update when the switch is updated (version at-time-of-linking)
-  switch_id    int not null,
-  switch_joint int not null,
-  primary key (node_id, node_version, switch_id, switch_joint),
-  constraint location_track_node_switch_node_version_fkey foreign key (node_id, node_version) references layout.location_track_node_version (id, version)
-);
-comment on table layout.location_track_node_switch_version is 'Versioned 1-to-many linking of switch nodes to location track node';
+-- create table layout.location_track_node_switch_version
+-- (
+--   node_id      int not null,
+--   node_version int not null,
+--   -- TODO: We could make this a versioned link that does not update when the switch is updated (version at-time-of-linking)
+--   switch_id    int not null,
+--   switch_joint int not null,
+--   primary key (node_id, node_version, switch_id, switch_joint),
+--   constraint location_track_node_switch_node_version_fkey foreign key (node_id, node_version) references layout.location_track_node_version (id, version)
+-- );
+-- comment on table layout.location_track_node_switch_version is 'Versioned 1-to-many linking of switch nodes to location track node';
 
 create table layout.location_track_edge
 (
   id            int primary key generated always as identity,
-  start_node_id int                             not null references layout.location_track_node (id),
-  end_node_id   int                             not null references layout.location_track_node (id),
+  edge_id int not null references layout.edge,
+--   start_node_id int                             not null references layout.location_track_node (id),
+--   end_node_id   int                             not null references layout.location_track_node (id),
   bounding_box  postgis.geometry(polygon, 3067) null,
   segment_count int                             not null,
   length        decimal(13, 6)                  not null
@@ -71,19 +120,19 @@ comment on table layout.location_track_edge_segment_version is 'A versioned geom
 
 -- Ref-tables for 1-many links in locationtrack <-> node/edge
 
--- This is a direct version-table under location_track versioning
-create table layout.location_track_node_ref_version
-(
-  location_track_id      int not null,
-  location_track_version int not null,
-  node_index             int not null,
-  node_id                int not null,
-  node_version           int not null,
-  constraint location_track_node_ref_node_fkey
-    foreign key (node_id, node_version) references layout.location_track_node_version (id, version)
-      deferrable initially deferred
-);
-comment on table layout.location_track_node_ref_version is 'Versioned 1-to-many linking for locationtrack nodes';
+-- -- This is a direct version-table under location_track versioning
+-- create table layout.location_track_node_ref_version
+-- (
+--   location_track_id      int not null,
+--   location_track_version int not null,
+--   node_index             int not null,
+--   node_id                int not null,
+--   node_version           int not null,
+--   constraint location_track_node_ref_node_fkey
+--     foreign key (node_id, node_version) references layout.location_track_node_version (id, version)
+--       deferrable initially deferred
+-- );
+-- comment on table layout.location_track_node_ref_version is 'Versioned 1-to-many linking for locationtrack nodes';
 
 -- This is a direct version-table under location_track versioning
 create table layout.location_track_edge_ref_version
