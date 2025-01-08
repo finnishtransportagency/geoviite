2ee34782fafbd9ef4508ad6a0843c4a0539ba226
Kari Surakka <kari.surakka@solita.fi>
2025-01-08 15:47:04 +0200

    Berry pick icon features for menu component from another branch

--- ui/src/vayla-design-lib/menu/menu.tsx
+++ ui/src/vayla-design-lib/menu/menu.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import styles from './menu.scss';
 import { CloseableModal, OpenTowards } from 'vayla-design-lib/closeable-modal/closeable-modal';
 import { createClassName } from 'vayla-design-lib/utils';
+import { IconColor, IconComponent, IconSize } from 'vayla-design-lib/icon/Icon';
 
 export type MenuOption = MenuSelectOption | MenuDividerOption;
 
@@ -14,11 +15,16 @@ export type MenuSelectOption = {
     name: string;
     onSelect: () => void;
     closingBehaviour: MenuClosingBehaviour;
+    icon: IconComponent | undefined;
 } & OptionBase;
 
 export type MenuDividerOption = {
     type: 'DIVIDER';
 };
+
+export function isMenuSelectOption(item: MenuOption): item is MenuSelectOption {
+    return item.type == 'SELECT';
+}
 
 export const menuOption = (
     onSelect: () => void,
@@ -26,6 +32,7 @@ export const menuOption = (
     qaId: string,
     disabled: boolean = false,
     closingBehaviour: MenuClosingBehaviour = 'CLOSE_AFTER_SELECT',
+    icon: IconComponent | undefined = undefined,
 ): MenuSelectOption => ({
     type: 'SELECT',
     onSelect,
@@ -33,6 +40,7 @@ export const menuOption = (
     disabled,
     closingBehaviour,
     qaId,
+    icon,
 });
 
 export const menuDivider = (): MenuDividerOption => ({
@@ -58,9 +66,17 @@ export const Menu = function ({
 }: MenuProps) {
     const { height: offsetY } = positionRef.current?.getBoundingClientRect() ?? { height: 0 };
 
+    const hasIcons = items.some((item) => isMenuSelectOption(item) && item.icon);
+
+    const menuClassName = createClassName(
+        styles['menu'],
+        className,
+        hasIcons && styles['menu--has-icons'],
+    );
+
     return (
         <CloseableModal
-            className={createClassName(styles['menu'], className)}
+            className={menuClassName}
             onClickOutside={onClickOutside}
             positionRef={positionRef}
             openTowards={opensTowards}
@@ -87,6 +103,16 @@ export const Menu = function ({
                                         }
                                     }
                                 }}>
+                                {hasIcons && (
+                                    <div className="menu__icon">
+                                        {isMenuSelectOption(item) && item.icon && (
+                                            <item.icon
+                                                size={IconSize.SMALL}
+                                                color={IconColor.INHERIT}
+                                            />
+                                        )}
+                                    </div>
+                                )}
                                 {item.name}
                             </li>
                         );
