3b42e99e545db88e0119c2f29554256e8df0f24e
Jyrki Jakobsson <jyrki.jakobsson@solita.fi>
2025-01-22 15:07:13 +0200

    GVT-2947 Add switch joints that were missing in init data and later used due to incomplete validation

--- infra/src/main/resources/db/migration/prod/V107__fix_switch_joint_history.sql
+++ infra/src/main/resources/db/migration/prod/V107__fix_switch_joint_history.sql
@@ -13,7 +13,7 @@ with
       where a.change_user = 'CSV_IMPORT'
   ),
   referenced_joints as (
-    select distinct on (id, joint) from (
+    select distinct on (id, joint) * from (
       select switch_id id, switch_start_joint_number joint, postgis.st_startpoint(geometry) as location from segments
       union all
       select switch_id id, switch_end_joint_number joint, postgis.st_endpoint(geometry) as location from segments
@@ -54,4 +54,29 @@ select
     left join switches s on s.id = rj.id
   where (not rj.joint = any (s.switch_joints))
     and (rj.joint = any (s.structure_joints));
+;
+
+-- These are not referenced in the init-data, but the missing joints are relevant in later versions
+-- where Geoviite did not verify the existence of the joints sufficiently. In each case, only the init version is
+-- actually missing the joint.
+insert into layout.switch_version_joint(switch_id, switch_layout_context_id, switch_version, number, location)
+values
+-- Switch 19 (missing joint 2) has not moved at all and -> pick the location from the first version that has it
+  (19, 'main_official', 1, 2, postgis.st_point(569642.4889834807,7173613.320921371, 3067)),
+-- Switch 5588 has not significantly moved since init (<1m) -> pick missing joint locations from the first version that has them
+  (5588, 'main_official', 1, 2, postgis.st_point(428053.3141959699,7208463.118335502, 3067)),
+  (5588, 'main_official', 1, 3, postgis.st_point(428058.7865148145,7208454.438310648, 3067)),
+  (5588, 'main_official', 1, 4, postgis.st_point(428051.6139379415,7208462.43349126, 3067)),
+  (5588, 'main_official', 1, 5, postgis.st_point(428063.60367248335,7208437.572723881, 3067)),
+  (5588, 'main_official', 1, 6, postgis.st_point(428059.86747912335,7208446.848545095, 3067)),
+-- Switch 5510 is RR which initially came with all joints in one position -> pick location for missing joints from the existing one
+  (5510, 'main_official', 1, 1, postgis.st_point(483824.65133276046,6747766.200798029, 3067)),
+  (5510, 'main_official', 1, 2, postgis.st_point(483824.65133276046,6747766.200798029, 3067)),
+  (5510, 'main_official', 1, 3, postgis.st_point(483824.65133276046,6747766.200798029, 3067)),
+  (5510, 'main_official', 1, 4, postgis.st_point(483824.65133276046,6747766.200798029, 3067)),
+-- OPTIONAL: Switch 5511 is RR which initially came with all joints in one position -> pick location for missing joints from the existing one
+  (5511, 'main_official', 1, 1, postgis.st_point(483587.25074875564,6747744.423612414, 3067)),
+  (5511, 'main_official', 1, 2, postgis.st_point(483587.25074875564,6747744.423612414, 3067)),
+  (5511, 'main_official', 1, 3, postgis.st_point(483587.25074875564,6747744.423612414, 3067)),
+  (5511, 'main_official', 1, 4, postgis.st_point(483587.25074875564,6747744.423612414, 3067))
 ;
