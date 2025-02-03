8f7ed1f5b2039e0d0fe15ed720b92b708cc15242
Ari Rahikkala <ari.rahikkala@solita.fi>
2025-02-03 14:56:00 +0200

    GVT-2976 Recreate timed fetch functions

---
+++ infra/src/main/resources/db/migration/prod/V110_recreate_timed_fetch_functions.sql
@@ -0,0 +1,7 @@
+select common.create_timed_fetch_function('layout', 'track_number');
+select common.create_timed_fetch_function('layout', 'reference_line');
+select common.create_timed_fetch_function('layout', 'location_track');
+select common.create_timed_fetch_function('layout', 'alignment');
+select common.create_timed_fetch_function('layout', 'switch');
+select common.create_timed_fetch_function('layout', 'km_post');
+
