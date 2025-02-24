c187d4687ecf08b00428eb0c02c0bf07dd6ef876
solita-mrunsten <128050934+solita-mrunsten@users.noreply.github.com>
2025-02-24 19:31:24 +0200

    GVT-3003 (part 2): Fix eye icon to be a button instead of a span

--- ui/src/geoviite-design-lib/eye/eye.tsx
+++ ui/src/geoviite-design-lib/eye/eye.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
 import styles from './eye.scss';
 import CircularProgress from 'vayla-design-lib/progress/circular-progress';
-import { IconColor, Icons } from 'vayla-design-lib/icon/Icon';
+import { IconColor, Icons, IconSize } from 'vayla-design-lib/icon/Icon';
 import { createClassName } from 'vayla-design-lib/utils';
+import { Button, ButtonSize, ButtonVariant } from 'vayla-design-lib/button/button';
 
 type EyeProps = {
     visibility?: boolean;
@@ -16,14 +17,29 @@ export const Eye: React.FC<EyeProps> = ({
     onVisibilityToggle,
     disabled = false,
 }) => {
-    const className = createClassName(
-        styles['eye'],
+    const iconClassName = createClassName(
+        styles['eye-icon'],
         visibility && styles['eye--visible'],
         disabled && styles['eye--disabled'],
     );
+
     return (
-        <span className={className} onClick={onVisibilityToggle}>
-            {fetchingContent ? <CircularProgress /> : <Icons.Eye color={IconColor.INHERIT} />}
+        <span className={styles['eye-container']}>
+            {fetchingContent ? (
+                <CircularProgress />
+            ) : (
+                <Button
+                    size={ButtonSize.SMALL}
+                    onClick={onVisibilityToggle}
+                    icon={Icons.Eye}
+                    iconProps={{
+                        size: IconSize.MEDIUM,
+                        color: IconColor.INHERIT,
+                        extraClassName: iconClassName,
+                    }}
+                    variant={ButtonVariant.GHOST}
+                />
+            )}
         </span>
     );
 };
