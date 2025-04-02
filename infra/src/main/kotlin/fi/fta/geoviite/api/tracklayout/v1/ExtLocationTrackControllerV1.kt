0955b1a2dc051f16e8689fcdff61b45953ff9ced
solita-mrunsten <128050934+solita-mrunsten@users.noreply.github.com>
2025-04-02 16:58:49 +0300

    wip: single location track fetch works

--- infra/src/main/kotlin/fi/fta/geoviite/api/tracklayout/v1/ExtLocationTrackControllerV1.kt
+++ infra/src/main/kotlin/fi/fta/geoviite/api/tracklayout/v1/ExtLocationTrackControllerV1.kt
@@ -6,6 +6,8 @@ import fi.fta.geoviite.infra.authorization.AUTH_API_GEOMETRY
 import fi.fta.geoviite.infra.common.Oid
 import fi.fta.geoviite.infra.tracklayout.LocationTrack
 import io.swagger.v3.oas.annotations.tags.Tag
+import fi.fta.geoviite.infra.common.Srid
+import fi.fta.geoviite.infra.tracklayout.LAYOUT_SRID
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.security.access.prepost.PreAuthorize
@@ -15,15 +17,44 @@ import org.springframework.web.bind.annotation.PathVariable
 @PreAuthorize(AUTH_API_GEOMETRY)
 @GeoviiteExtApiController([])
 @Tag(name = "Rataverkon paikannuspohja V1")
-class ExtLocationTrackControllerV1 {
+class ExtLocationTrackControllerV1(private val extLocationTrackService: ExtLocationTrackServiceV1) {
 
     val logger: Logger = LoggerFactory.getLogger(this::class.java)
 
-    @GetMapping(
-        "/geoviite/paikannuspohja/v1/sijaintiraiteet/{$LOCATION_TRACK_OID_PARAM}",
-        "/geoviite/dev/paikannuspohja/v1/sijaintiraiteet/{$LOCATION_TRACK_OID_PARAM}",
-    )
-    fun extGetLocationTrack(@PathVariable(LOCATION_TRACK_OID_PARAM) oid: Oid<LocationTrack>): String {
-        return "Scaffolding"
+    //    @GetMapping(
+    //        "/geoviite/paikannuspohja/v1/sijaintiraiteet/{$LOCATION_TRACK_OID_PARAM}",
+    //        "/geoviite/paikannuspohja/v1/sijaintiraiteet/{$LOCATION_TRACK_OID_PARAM}/",
+    //    )
+    //    fun singleTrackCenterLineGeometry(
+    //        @PathVariable(LOCATION_TRACK_OID_PARAM) locationTrackOid: ApiRequestStringV1,
+    //        @RequestParam(TRACK_NETWORK_VERSION) trackNetworkVersion: ApiRequestStringV1?,
+    //        @RequestParam(MODIFICATIONS_FROM_VERSION) modificationsFromVersion: ApiRequestStringV1?,
+    //        @RequestParam(COORDINATE_SYSTEM_PARAM, required = false) coordinateSystem: ApiRequestStringV1?,
+    //    ): CenterLineGeometryResponseV1 {
+    //        val translation = localizationService.getLocalization(LocalizationLanguage.FI)
+    //
+    //        val (validationErrors, validRequest) =
+    //            LocationTrackRequestV1(locationTrackOid, coordinateSystem).let(centerLineGeometryService::validate)
+    //
+    //        return validRequest?.let { processValidatedRequest(translation, validRequest) }
+    //            ?: createErrorResponse(translation, validationErrors)
+    //    }
+
+    @GetMapping("/geoviite/paikannuspohja/v1/sijaintiraiteet/{$LOCATION_TRACK_OID_PARAM}")
+    fun locationTrack(
+        @PathVariable(LOCATION_TRACK_OID_PARAM) oid: Oid<LocationTrack>,
+        @RequestParam(COORDINATE_SYSTEM_PARAM, required = false) coordinateSystem: Srid = LAYOUT_SRID,
+    ): ExtLocationTrackResponseV1 {
+        return extLocationTrackService.locationTrackResponse(oid, coordinateSystem)
     }
+
+    //    @GetMapping(
+    //        "/geoviite/paikannuspohja/v1/sijaintiraiteet/{$LOCATION_TRACK_OID_PARAM}",
+    //        params = [MODIFICATIONS_FROM_VERSION],
+    //    )
+    //    fun locationTrackModifications(
+    //        @PathVariable(LOCATION_TRACK_OID_PARAM) locationTrackOid: Oid<LocationTrack>,
+    //        @RequestParam(COORDINATE_SYSTEM_PARAM, required = false) coordinateSystem: Srid,
+    //        @RequestParam(MODIFICATIONS_FROM_VERSION, required = true) modificationsFromVersion: Uuid<Publication>,
+    //    ): ExtModifiedLocationTrackResponseV1 {}
 }
---
+++ infra/src/main/kotlin/fi/fta/geoviite/api/tracklayout/v1/ExtTrackLayoutExceptionsV1.kt
@@ -0,0 +1,35 @@
+package fi.fta.geoviite.api.tracklayout.v1
+
+import fi.fta.geoviite.infra.error.HasLocalizedMessage
+import fi.fta.geoviite.infra.localization.LocalizationKey
+import fi.fta.geoviite.infra.localization.LocalizationParams
+import java.lang.RuntimeException
+import org.springframework.http.HttpStatus
+
+sealed class ExtApiExceptionV1(
+    val status: HttpStatus,
+    message: String,
+    cause: Throwable? = null,
+    override val localizationKey: LocalizationKey,
+    override val localizationParams: LocalizationParams = LocalizationParams.empty,
+) : RuntimeException(message, cause), HasLocalizedMessage {
+    constructor(
+        status: HttpStatus,
+        message: String,
+        cause: Throwable?,
+        localizedMessageKey: String,
+        localizedMessageParams: LocalizationParams = LocalizationParams.empty,
+    ) : this(status, message, cause, LocalizationKey(localizedMessageKey), localizedMessageParams)
+}
+
+class ExtOidNotFoundExceptionV1(
+    message: String,
+    cause: Throwable? = null,
+    localizedMessageKey: String = "ext-api.track-layout.v1.error.oid-not-found",
+) : ExtApiExceptionV1(HttpStatus.NOT_FOUND, "oid not found: $message", cause, localizedMessageKey)
+
+class ExtGeocodingFailedV1(
+    message: String,
+    cause: Throwable? = null,
+    localizedMessageKey: String = "ext-api.track-layout.v1.error.geocoding-failed",
+) : ExtApiExceptionV1(HttpStatus.INTERNAL_SERVER_ERROR, "geocoding failed: $message", cause, localizedMessageKey)
