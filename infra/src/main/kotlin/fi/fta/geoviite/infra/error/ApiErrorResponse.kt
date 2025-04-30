4dd38c40af62b790298d2afbe38408c16741f282
solita-mrunsten <128050934+solita-mrunsten@users.noreply.github.com>
2025-04-30 08:05:16 +0300

    wip

--- infra/src/main/kotlin/fi/fta/geoviite/infra/error/ApiErrorResponse.kt
+++ infra/src/main/kotlin/fi/fta/geoviite/infra/error/ApiErrorResponse.kt
@@ -1,11 +1,10 @@
 package fi.fta.geoviite.infra.error
 
-import com.fasterxml.jackson.annotation.JsonProperty
 import fi.fta.geoviite.infra.localization.LocalizationKey
 import fi.fta.geoviite.infra.localization.LocalizationParams
 import java.time.Instant
 
-sealed class GeoviiteErrorResponse
+open class GeoviiteErrorResponse
 
 data class ApiErrorResponse(
     val messageRows: List<String>,
@@ -13,10 +12,4 @@ data class ApiErrorResponse(
     val localizationParams: LocalizationParams,
     val correlationId: String,
     val timestamp: Instant = Instant.now(),
-) : GeoviiteErrorResponse()
-
-data class ExtApiErrorResponseV1(
-    @JsonProperty("virheviesti") val errorMessage: String,
-    @JsonProperty("korrelaatiotunnus") val correlationId: String,
-    @JsonProperty("aikaleima") val timestamp: Instant = Instant.now(),
 ) : GeoviiteErrorResponse()
