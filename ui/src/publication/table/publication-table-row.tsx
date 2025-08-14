4ee678ad8e71b590749e0627432aeda62a3e9d50
Kari Surakka <kari.surakka@solita.fi>
2025-08-14 15:05:54 +0300

    Remove expand functionality from publication message

--- ui/src/publication/table/publication-table-row.tsx
+++ ui/src/publication/table/publication-table-row.tsx
@@ -7,12 +7,9 @@ import {
     PublicationTableItem,
 } from 'publication/publication-model';
 import styles from './publication-table.scss';
-import { Icons } from 'vayla-design-lib/icon/Icon';
 import { createClassName } from 'vayla-design-lib/utils';
-import { Button, ButtonSize, ButtonVariant } from 'vayla-design-lib/button/button';
 import { AccordionToggle } from 'vayla-design-lib/accordion-toggle/accordion-toggle';
 import { PublicationTableDetails } from 'publication/table/publication-table-details';
-import { first } from 'utils/array-utils';
 import { SearchItemType, SearchItemValue } from 'tool-bar/search-dropdown';
 import { SearchablePublicationLogItem } from 'publication/log/publication-log';
 import { AnchorLink } from 'geoviite-design-lib/link/anchor-link';
@@ -60,19 +57,6 @@ const PublicationTableRow: React.FC<PublicationTableRowProps> = ({
     displayItemHistory,
 }) => {
     const { t } = useTranslation();
-    const messageRows = message.split('\n');
-    const [messageExpanded, setMessageExpanded] = React.useState(false);
-    const contentClassNames = createClassName(
-        styles['publication-table__message-content'],
-        messageExpanded
-            ? styles['publication-table__message-content--expanded']
-            : styles['publication-table__message-content--collapsed'],
-    );
-    const chevronClassNames = createClassName(
-        styles['publication-table__message-icon'],
-        messageExpanded ? styles['publication-table__message-icon--open'] : undefined,
-    );
-
     const rowClassNames = createClassName(
         'publication-table__row',
         detailsVisible && styles['publication-table__row--details-are-visible'],
@@ -114,18 +98,9 @@ const PublicationTableRow: React.FC<PublicationTableRowProps> = ({
                 <td>{formatDateFull(publicationTime)}</td>
                 <td>{publicationUser}</td>
                 <td className={styles['publication-table__message-column']} title={message}>
-                    <Button
-                        className={chevronClassNames}
-                        icon={Icons.Down}
-                        variant={ButtonVariant.GHOST}
-                        size={ButtonSize.SMALL}
-                        onClick={() => setMessageExpanded(!messageExpanded)}
-                    />
-                    <div className={contentClassNames}>
-                        <AnchorLink onClick={() => displaySinglePublication(publicationId)}>
-                            {messageExpanded ? message : first(messageRows)}
-                        </AnchorLink>
-                    </div>
+                    <AnchorLink onClick={() => displaySinglePublication(publicationId)}>
+                        {message}
+                    </AnchorLink>
                 </td>
                 <td>{ratkoPushTime ? formatDateFull(ratkoPushTime) : t('no')}</td>
             </tr>
