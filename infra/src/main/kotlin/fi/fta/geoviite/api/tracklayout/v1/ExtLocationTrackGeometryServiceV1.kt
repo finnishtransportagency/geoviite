b37fe17e1ad07c264ab18d27e7c54e1855044eed
Jyrki Jakobsson <jyrki.jakobsson@solita.fi>
2025-11-20 13:50:21 +0200

    GVT-3098 Generalize addresspoint diff functions + add geometry modifications API for track numbers
    # Conflicts:
    #	infra/src/main/kotlin/fi/fta/geoviite/api/tracklayout/v1/ExtLocationTrackGeometryServiceV1.kt

--- infra/src/main/kotlin/fi/fta/geoviite/api/tracklayout/v1/ExtLocationTrackGeometryServiceV1.kt
+++ infra/src/main/kotlin/fi/fta/geoviite/api/tracklayout/v1/ExtLocationTrackGeometryServiceV1.kt
@@ -6,10 +6,8 @@ import fi.fta.geoviite.infra.common.LayoutBranch
 import fi.fta.geoviite.infra.common.LayoutBranchType
 import fi.fta.geoviite.infra.common.Oid
 import fi.fta.geoviite.infra.common.Srid
-import fi.fta.geoviite.infra.common.TrackMeter
 import fi.fta.geoviite.infra.common.Uuid
 import fi.fta.geoviite.infra.geocoding.AddressFilter
-import fi.fta.geoviite.infra.geocoding.AddressPoint
 import fi.fta.geoviite.infra.geocoding.AlignmentAddresses
 import fi.fta.geoviite.infra.geocoding.GeocodingDao
 import fi.fta.geoviite.infra.geocoding.GeocodingService
@@ -18,7 +16,6 @@ import fi.fta.geoviite.infra.publication.Publication
 import fi.fta.geoviite.infra.publication.PublicationComparison
 import fi.fta.geoviite.infra.publication.PublicationDao
 import fi.fta.geoviite.infra.publication.PublicationService
-import fi.fta.geoviite.infra.tracklayout.LAYOUT_COORDINATE_DELTA
 import fi.fta.geoviite.infra.tracklayout.LAYOUT_SRID
 import fi.fta.geoviite.infra.tracklayout.LayoutAlignmentDao
 import fi.fta.geoviite.infra.tracklayout.LocationTrack
@@ -31,12 +28,12 @@ import org.springframework.beans.factory.annotation.Autowired
 class ExtLocationTrackGeometryServiceV1
 @Autowired
 constructor(
+    private val publicationService: PublicationService,
+    private val publicationDao: PublicationDao,
     private val geocodingService: GeocodingService,
     private val geocodingDao: GeocodingDao,
     private val locationTrackDao: LocationTrackDao,
     private val alignmentDao: LayoutAlignmentDao,
-    private val publicationService: PublicationService,
-    private val publicationDao: PublicationDao,
 ) {
     fun getExtLocationTrackGeometry(
         oid: Oid<LocationTrack>,
@@ -104,124 +101,9 @@ constructor(
                     trackLayoutVersionTo = publications.to.uuid,
                     locationTrackOid = oid,
                     coordinateSystem = coordinateSystem,
-                    trackIntervals = createModifiedTrackIntervals(oldPoints, newPoints, coordinateSystem),
+                    trackIntervals = createModifiedCenterLineIntervals(oldPoints, newPoints, coordinateSystem),
                 )
             }
-    }
-
-    private fun createModifiedTrackIntervals(
-        oldPoints: AlignmentAddresses<LocationTrackM>?,
-        newPoints: AlignmentAddresses<LocationTrackM>?,
-        coordinateSystem: Srid,
-    ): List<ExtCenterLineTrackIntervalV1> =
-        when {
-            oldPoints == null && newPoints == null -> error("Must have some points to compare")
-            oldPoints == null -> {
-                // Full new interval was added
-                listOf(toExtInterval(newPoints!!, coordinateSystem))
-            }
-            newPoints == null -> {
-                // All points removed on the original interval
-                listOf(
-                    ExtCenterLineTrackIntervalV1(
-                        startAddress = oldPoints.startPoint.address.formatFixedDecimals(3),
-                        endAddress = oldPoints.endPoint.address.formatFixedDecimals(3),
-                        addressPoints = emptyList(),
-                    )
-                )
-            }
-            else -> {
-                val changedIntervals = mutableListOf<ExtCenterLineTrackIntervalV1>()
-                val rangeBuilder = RangeBuilder(coordinateSystem)
-                var oldIndex = 0
-                var newIndex = 0
-                while (oldIndex < oldPoints.allPoints.lastIndex || newIndex < newPoints.allPoints.lastIndex) {
-                    val old = oldPoints.allPoints.getOrNull(oldIndex)
-                    val new = newPoints.allPoints.getOrNull(newIndex)
-                    when {
-                        old == null && new == null -> {
-                            error("Loop failed to end despite both lists being fully processed")
-                        }
-                        old == null -> { // Reached the end of the old points -> rest of new different
-                            rangeBuilder.extendWithPoints(
-                                newPoints.allPoints.subList(newIndex, newPoints.allPoints.size)
-                            )
-                            newIndex = newPoints.allPoints.size
-                        }
-                        new == null -> { // Reached the end of the new points -> rest of old different
-                            rangeBuilder.extendAsEmpty(old.address)
-                            rangeBuilder.extendAsEmpty(oldPoints.allPoints.last().address)
-                            oldIndex = oldPoints.allPoints.size
-                        }
-                        else ->
-                            when {
-                                old.address < new.address -> {
-                                    rangeBuilder.extendAsEmpty(old.address)
-                                    oldIndex++
-                                }
-                                old.address > new.address -> {
-                                    rangeBuilder.extendWithPoint(new)
-                                    newIndex++
-                                }
-                                else -> {
-                                    if (old.point.isSame(new.point, LAYOUT_COORDINATE_DELTA)) {
-                                        rangeBuilder.buildAndReset()?.let(changedIntervals::add)
-                                    } else {
-                                        rangeBuilder.extendWithPoint(new)
-                                    }
-                                    oldIndex++
-                                    newIndex++
-                                }
-                            }
-                    }
-                }
-                // Finish range if one still exists
-                rangeBuilder.buildAndReset()?.let(changedIntervals::add)
-                changedIntervals
-            }
-        }
-
-    private class RangeBuilder(val coordinateSystem: Srid) {
-        private var start: TrackMeter? = null
-        private var end: TrackMeter? = null
-        private val points = mutableListOf<AddressPoint<LocationTrackM>>()
-
-        fun extendAsEmpty(address: TrackMeter) {
-            if (start == null) start = address
-            end = address
-        }
-
-        fun extendWithPoint(addressPoint: AddressPoint<LocationTrackM>) {
-            if (start == null) start = addressPoint.address
-            end = addressPoint.address
-            points.add(addressPoint)
-        }
-
-        fun extendWithPoints(addressPoints: List<AddressPoint<LocationTrackM>>) {
-            if (addressPoints.isNotEmpty()) {
-                if (start == null) start = addressPoints.first().address
-                end = addressPoints.last().address
-                points.addAll(addressPoints)
-            }
-        }
-
-        fun buildAndReset(): ExtCenterLineTrackIntervalV1? {
-            val startAddress = start?.formatFixedDecimals(3)
-            val endAddress = end?.formatFixedDecimals(3)
-            val addressPoints = points.map { addressPoint -> toExtAddressPoint(addressPoint, coordinateSystem) }
-            reset()
-            return if (startAddress != null && endAddress != null) {
-                ExtCenterLineTrackIntervalV1(startAddress, endAddress, addressPoints)
-            } else {
-                null
-            }
-        }
-
-        private fun reset() {
-            start = null
-            end = null
-            points.clear()
-        }
     }
 
     private fun createGeometryResponse(
@@ -252,17 +134,6 @@ constructor(
                 )
             }
     }
-
-    private fun toExtInterval(
-        addressPoints: AlignmentAddresses<LocationTrackM>,
-        coordinateSystem: Srid,
-    ): ExtCenterLineTrackIntervalV1 =
-        ExtCenterLineTrackIntervalV1(
-            startAddress = addressPoints.startPoint.address.formatFixedDecimals(3),
-            endAddress = addressPoints.endPoint.address.formatFixedDecimals(3),
-            addressPoints =
-                addressPoints.allPoints.map { addressPoint -> toExtAddressPoint(addressPoint, coordinateSystem) },
-        )
 
     private fun getAddressPoints(
         branch: LayoutBranch,
