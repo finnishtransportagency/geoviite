40c5888ef5b7bc76b0b279f97986ed8185791d70
Jyrki Jakobsson <jyrki.jakobsson@solita.fi>
2026-02-03 15:02:04 +0200

    GVT-3401 Refactor station links to include rowversions

--- infra/src/main/kotlin/fi/fta/geoviite/infra/tracklayout/StationLinkService.kt
+++ infra/src/main/kotlin/fi/fta/geoviite/infra/tracklayout/StationLinkService.kt
@@ -3,6 +3,7 @@ package fi.fta.geoviite.infra.tracklayout
 import fi.fta.geoviite.infra.aspects.GeoviiteService
 import fi.fta.geoviite.infra.common.IntId
 import fi.fta.geoviite.infra.common.LayoutBranch
+import fi.fta.geoviite.infra.common.LayoutContext
 import fi.fta.geoviite.infra.geography.calculateDistance
 import fi.fta.geoviite.infra.util.produceIf
 import java.time.Instant
@@ -11,6 +12,7 @@ import java.time.Instant
 class StationLinkService(
     private val operationalPointDao: OperationalPointDao,
     private val layoutSwitchDao: LayoutSwitchDao,
+    private val layoutTrackNumberDao: LayoutTrackNumberDao,
     private val locationTrackService: LocationTrackService,
 ) {
 
@@ -30,6 +32,7 @@ class StationLinkService(
     }
 
     private data class TrackStationConnection(
+        val trackVersion: LayoutRowVersion<LocationTrack>,
         val track: LocationTrack,
         val op1Point: TrackStationPoint,
         val op2Point: TrackStationPoint,
@@ -45,30 +48,59 @@ class StationLinkService(
     }
 
     fun getStationLinks(branch: LayoutBranch, moment: Instant): List<StationLink> {
-        val operationalPoints =
-            operationalPointDao.listOfficialAtMoment(branch, moment).associateBy(OperationalPoint::id)
+        val operationalPoints = operationalPointDao.listOfficialAtMoment(branch, moment)
+        val switches = layoutSwitchDao.listOfficialAtMoment(branch, moment)
+        val tracksWithGeometry = locationTrackService.listOfficialWithGeometryAtMoment(branch, moment)
+        val trackNumberIds = tracksWithGeometry.map { it.first.trackNumberId }.distinct()
+        val trackNumberVersions = layoutTrackNumberDao.fetchManyOfficialVersionsAtMoment(branch, trackNumberIds, moment)
+        return calculateStationLinks(operationalPoints, switches, tracksWithGeometry, trackNumberVersions)
+    }
+
+    fun getStationLinks(context: LayoutContext): List<StationLink> {
+        val operationalPoints = operationalPointDao.list(context, includeDeleted = false)
+        val switches = layoutSwitchDao.list(context, includeDeleted = false)
+        val tracksWithGeometry = locationTrackService.listWithGeometries(context, includeDeleted = false)
+        val trackNumberIds = tracksWithGeometry.map { it.first.trackNumberId }.distinct()
+        val trackNumberVersions = layoutTrackNumberDao.fetchVersions(context, trackNumberIds)
+        return calculateStationLinks(operationalPoints, switches, tracksWithGeometry, trackNumberVersions)
+    }
+
+    private fun calculateStationLinks(
+        operationalPoints: List<OperationalPoint>,
+        switches: List<LayoutSwitch>,
+        tracksWithGeometry: List<Pair<LocationTrack, LocationTrackGeometry>>,
+        trackNumberVersions: List<LayoutRowVersion<LayoutTrackNumber>>,
+    ): List<StationLink> {
+        val operationalPointsById = operationalPoints.associateBy { it.id as IntId }
+        fun getOp(id: IntId<OperationalPoint>): OperationalPoint =
+            requireNotNull(operationalPointsById[id]) { "Operational point $id not found" }
+        val trackNumberVersionById = trackNumberVersions.associateBy { it.id }
         val switchIdToOpId =
-            layoutSwitchDao
-                .listOfficialAtMoment(branch, moment)
-                .mapNotNull { s -> s.operationalPointId?.let { s.id as IntId to it } }
-                .associate { it }
-        return locationTrackService
-            .listOfficialWithGeometryAtMoment(branch, moment)
-            .flatMap { (track, geom) -> resolveTrackConnections(track, geom, switchIdToOpId) }
+            switches.mapNotNull { s -> s.operationalPointId?.let { s.id as IntId to it } }.associate { it }
+
+        return tracksWithGeometry
+            .flatMap { (track, geom) ->
+                val trackVersion = track.getVersionOrThrow()
+                resolveTrackConnections(trackVersion, track, geom, switchIdToOpId)
+            }
             .groupBy { connection -> connection.stationLinkKey }
             .map { (key, connections) ->
-                val op1 = requireNotNull(operationalPoints[key.op1Id]) { "Operational point ${key.op1Id} not found" }
-                val op2 = requireNotNull(operationalPoints[key.op2Id]) { "Operational point ${key.op2Id} not found" }
-                val link = connections.minBy { it.length }
+                val op1 = getOp(key.op1Id)
+                val op2 = getOp(key.op2Id)
+                val shortestLink = connections.minBy { it.length }
+                val trackNumberVersion =
+                    requireNotNull(trackNumberVersionById[key.trackNumberId]) {
+                        "Track number version ${key.trackNumberId} not found"
+                    }
                 StationLink(
-                    trackNumberId = key.trackNumberId,
-                    startOperationalPointId = key.op1Id,
-                    endOperationalPointId = key.op2Id,
-                    locationTrackIds = connections.map { it.track.id as IntId },
+                    trackNumberVersion = trackNumberVersion,
+                    startOperationalPointVersion = op1.getVersionOrThrow(),
+                    endOperationalPointVersion = op2.getVersionOrThrow(),
+                    locationTrackVersions = connections.map { it.trackVersion },
                     length =
-                        link.length.distance +
-                            calculateDistance(op1, link.op1Point) +
-                            calculateDistance(op2, link.op2Point),
+                        shortestLink.length.distance +
+                            calculateDistance(op1, shortestLink.op1Point) +
+                            calculateDistance(op2, shortestLink.op2Point),
                 )
             }
     }
@@ -81,6 +113,7 @@ class StationLinkService(
         )
 
     private fun resolveTrackConnections(
+        trackVersion: LayoutRowVersion<LocationTrack>,
         track: LocationTrack,
         geometry: LocationTrackGeometry,
         switchIdToOpId: Map<IntId<LayoutSwitch>, IntId<OperationalPoint>>,
@@ -91,6 +124,6 @@ class StationLinkService(
             }
             .zipWithNext()
             .mapNotNull { (prev, next) ->
-                produceIf(prev.opId != next.opId) { TrackStationConnection(track, prev, next) }
+                produceIf(prev.opId != next.opId) { TrackStationConnection(trackVersion, track, prev, next) }
             }
 }
--- infra/src/test/kotlin/fi/fta/geoviite/infra/tracklayout/StationLinkServiceIT.kt
+++ infra/src/test/kotlin/fi/fta/geoviite/infra/tracklayout/StationLinkServiceIT.kt
@@ -22,76 +22,72 @@ class StationLinkServiceIT @Autowired constructor(private val stationLinkService
     }
 
     @Test
-    fun `getStationLinks returns correct links when fetched with timestamp`() {
+    fun `getStationLinks returns correct links`() {
         val moment0 = testDBService.getDbTime()
         stationLinkService.getStationLinks(LayoutBranch.main, moment0).also { links -> assertTrue(links.isEmpty()) }
+        stationLinkService.getStationLinks(mainOfficialContext.context).also { links -> assertTrue(links.isEmpty()) }
 
-        val tnId =
-            mainOfficialContext
-                .createLayoutTrackNumberAndReferenceLine(
-                    referenceLineGeometryOfPoints(Point(0.0, 0.0), Point(100.0, 0.0))
-                )
-                .id
-        val op1Id = mainOfficialContext.save(operationalPoint("OP1", location = Point(20.0, 0.0))).id
-        val op2Id = mainOfficialContext.save(operationalPoint("OP2", location = Point(50.0, 0.0))).id
-        val op3Id = mainOfficialContext.save(operationalPoint("OP3", location = Point(80.0, 0.0))).id
+        val tnVersion =
+            mainOfficialContext.createLayoutTrackNumberAndReferenceLine(
+                referenceLineGeometryOfPoints(Point(0.0, 0.0), Point(100.0, 0.0))
+            )
+        val op1Version = mainOfficialContext.save(operationalPoint("OP1", location = Point(20.0, 0.0)))
+        val op2Version = mainOfficialContext.save(operationalPoint("OP2", location = Point(50.0, 0.0)))
+        val op3Version = mainOfficialContext.save(operationalPoint("OP3", location = Point(80.0, 0.0)))
 
         // The switches connect to the operational points with the same number
-        val switch1Id = mainOfficialContext.save(switch(operationalPointId = op1Id)).id
-        val switch2Id = mainOfficialContext.save(switch(operationalPointId = op2Id)).id
-        val switch3Id = mainOfficialContext.save(switch(operationalPointId = op3Id)).id
+        val switch1Id = mainOfficialContext.save(switch(operationalPointId = op1Version.id)).id
+        val switch2Id = mainOfficialContext.save(switch(operationalPointId = op2Version.id)).id
+        val switch3Id = mainOfficialContext.save(switch(operationalPointId = op3Version.id)).id
 
         // Track 1 does links 1-2 and 2-3
-        val track1Id =
-            mainOfficialContext
-                .save(
-                    locationTrack(tnId),
-                    trackGeometry(
-                        // Edge 1 goes from switch1 to switch2 (not switch-internal geometry)
-                        edge(
-                            startOuterSwitch = switchLinkYV(switch1Id, 3),
-                            endOuterSwitch = switchLinkYV(switch2Id, 1),
-                            segments = listOf(segment(Point(30.0, 5.0), Point(40.0, 5.0))),
-                        ),
-                        // Edge 2 is switch2 internal geometry
-                        edge(
-                            startInnerSwitch = switchLinkYV(switch2Id, 1),
-                            endInnerSwitch = switchLinkYV(switch2Id, 2),
-                            segments = listOf(segment(Point(40.0, 5.0), Point(45.0, 5.0))),
-                        ),
-                        // Edge 3 goes from switch2 to switch3 (not switch-internal geometry)
-                        edge(
-                            startOuterSwitch = switchLinkYV(switch2Id, 2),
-                            endOuterSwitch = switchLinkYV(switch3Id, 1),
-                            segments = listOf(segment(Point(45.0, 5.0), Point(85.0, 5.0))),
-                        ),
+        val track1Version =
+            mainOfficialContext.save(
+                locationTrack(tnVersion.id),
+                trackGeometry(
+                    // Edge 1 goes from switch1 to switch2 (not switch-internal geometry)
+                    edge(
+                        startOuterSwitch = switchLinkYV(switch1Id, 3),
+                        endOuterSwitch = switchLinkYV(switch2Id, 1),
+                        segments = listOf(segment(Point(30.0, 5.0), Point(40.0, 5.0))),
                     ),
-                )
-                .id
+                    // Edge 2 is switch2 internal geometry
+                    edge(
+                        startInnerSwitch = switchLinkYV(switch2Id, 1),
+                        endInnerSwitch = switchLinkYV(switch2Id, 2),
+                        segments = listOf(segment(Point(40.0, 5.0), Point(45.0, 5.0))),
+                    ),
+                    // Edge 3 goes from switch2 to switch3 (not switch-internal geometry)
+                    edge(
+                        startOuterSwitch = switchLinkYV(switch2Id, 2),
+                        endOuterSwitch = switchLinkYV(switch3Id, 1),
+                        segments = listOf(segment(Point(45.0, 5.0), Point(85.0, 5.0))),
+                    ),
+                ),
+            )
 
         // Track 2 does the 2-3 link only and is shorter that the same link on track 1
-        val track2Id =
-            mainOfficialContext
-                .save(
-                    locationTrack(tnId),
-                    trackGeometry(
-                        // The only edge goes from switch2 to switch3 (not switch-internal geometry)
-                        edge(
-                            startOuterSwitch = switchLinkYV(switch2Id, 2),
-                            endOuterSwitch = switchLinkYV(switch3Id, 1),
-                            segments = listOf(segment(Point(46.0, 5.0), Point(84.0, 5.0))),
-                        )
-                    ),
-                )
-                .id
+        val track2Version =
+            mainOfficialContext.save(
+                locationTrack(tnVersion.id),
+                trackGeometry(
+                    // The only edge goes from switch2 to switch3 (not switch-internal geometry)
+                    edge(
+                        startOuterSwitch = switchLinkYV(switch2Id, 2),
+                        endOuterSwitch = switchLinkYV(switch3Id, 1),
+                        segments = listOf(segment(Point(46.0, 5.0), Point(84.0, 5.0))),
+                    )
+                ),
+            )
 
         val moment1 = testDBService.getDbTime()
-        stationLinkService.getStationLinks(LayoutBranch.main, moment1).also { links ->
+        val momentLinks = stationLinkService.getStationLinks(LayoutBranch.main, moment1)
+        momentLinks.also { links ->
             // Assert correct links are found, ignoring length (due to floating-point comparison)
             assertEquals(
                 listOf(
-                    StationLink(tnId, op1Id, op2Id, listOf(track1Id), 0.0),
-                    StationLink(tnId, op2Id, op3Id, listOf(track1Id, track2Id), 0.0),
+                    StationLink(tnVersion, op1Version, op2Version, listOf(track1Version), 0.0),
+                    StationLink(tnVersion, op2Version, op3Version, listOf(track1Version, track2Version), 0.0),
                 ),
                 links.map { it.copy(length = 0.0) },
             )
@@ -112,6 +108,10 @@ class StationLinkServiceIT @Autowired constructor(private val stationLinkService
                 LAYOUT_M_DELTA,
             )
         }
+
+        // Verify that the context-based fetch returns the exact same results as moment-based fetch
+        val contextLinks = stationLinkService.getStationLinks(mainOfficialContext.context)
+        assertEquals(momentLinks, contextLinks)
 
         // Verify that the 0-moment links is still empty
         stationLinkService.getStationLinks(LayoutBranch.main, moment0).also { links -> assertTrue(links.isEmpty()) }
