f2b4e04b8a106d750a4b31ddfa5d2c89ff9c2276
Ari Rahikkala <ari.rahikkala@solita.fi>
2026-02-18 09:31:25 +0200

    GVT-3486 Don't display draft OID edit field for published switches

--- ui/src/tool-panel/switch/dialog/switch-edit-dialog.tsx
+++ ui/src/tool-panel/switch/dialog/switch-edit-dialog.tsx
@@ -23,6 +23,7 @@ import {
     draftLayoutContext,
     LayoutContext,
     officialLayoutContext,
+    Oid,
     SwitchOwner,
     SwitchOwnerId,
     SwitchStructure,
@@ -35,6 +36,7 @@ import dialogStyles from 'geoviite-design-lib/dialog/dialog.scss';
 import {
     getSwitch,
     getSwitchesByName,
+    getSwitchOids,
     insertSwitch,
     updateSwitch,
 } from 'track-layout/layout-switch-api';
@@ -125,6 +127,7 @@ export const SwitchEditDialog = ({
     const [switchOwnerId, setSwitchOwnerId] = React.useState<SwitchOwnerId>();
     const [existingSwitch, setExistingSwitch] = React.useState<LayoutSwitch>();
     const [editingOid, setEditingOid] = React.useState(false);
+    const [publishedMainOid, setPublishedMainOid] = React.useState<Oid>();
     const firstInputRef = React.useRef<HTMLInputElement>(null);
     const isExistingSwitch = !!switchId;
 
@@ -153,15 +156,19 @@ export const SwitchEditDialog = ({
 
     React.useEffect(() => {
         if (isExistingSwitch) {
-            getSwitch(switchId, draftLayoutContext(layoutContext)).then((s) => {
-                if (s) {
-                    setExistingSwitch(s);
-                    setSwitchStateCategory(s.stateCategory);
-                    setSwitchName(s.name);
-                    setSwitchStructureId(s.switchStructureId);
-                    setTrapPoint(booleanToTrapPoint(s.trapPoint));
-                    if (s.draftOid) {
-                        setSwitchDraftOid(s.draftOid);
+            Promise.all([
+                getSwitch(switchId, draftLayoutContext(layoutContext)),
+                getSwitchOids(switchId, changeTime).then((oids) => oids['MAIN']),
+            ]).then(([loadedSwitch, loadedPublishedMainOid]) => {
+                setPublishedMainOid(loadedPublishedMainOid);
+                if (loadedSwitch) {
+                    setExistingSwitch(loadedSwitch);
+                    setSwitchStateCategory(loadedSwitch.stateCategory);
+                    setSwitchName(loadedSwitch.name);
+                    setSwitchStructureId(loadedSwitch.switchStructureId);
+                    setTrapPoint(booleanToTrapPoint(loadedSwitch.trapPoint));
+                    if (loadedSwitch.draftOid && !loadedPublishedMainOid) {
+                        setSwitchDraftOid(loadedSwitch.draftOid);
                         setEditingOid(true);
                     }
                     firstInputRef.current?.focus();
@@ -359,10 +366,8 @@ export const SwitchEditDialog = ({
                         <Heading size={HeadingSize.SUB}>
                             {t('switch-dialog.basic-info-heading')}
                         </Heading>
-                        {layoutContext.branch === 'MAIN' && (
+                        {layoutContext.branch === 'MAIN' && !publishedMainOid && (
                             <SwitchDraftOidField
-                                switchId={existingSwitch?.id}
-                                changeTime={changeTime}
                                 draftOid={switchDraftOid}
                                 setDraftOid={setSwitchDraftOid}
                                 setDraftOidExistsInRatko={setSwitchDraftOidExistsInRatko}
