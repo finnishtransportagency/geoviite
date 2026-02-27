354bf87d7b1d9f354b3a91e3172e9b53d181307e
Juuse Koivula <juuse.koivula@solita.fi>
2026-02-27 11:37:24 +0200

    GVT-3503: Remove exclamation point from app-bar's inframodel link GVT-3503: Show exclamation point if there is no connection to Ratko or if pushing the latest publication has failed

--- ui/src/app-bar/links/frontpage-link.tsx
+++ ui/src/app-bar/links/frontpage-link.tsx
@@ -1,8 +1,12 @@
 import * as React from 'react';
 import { NavLink } from 'react-router-dom';
-import { useTrackLayoutAppSelector } from 'store/hooks';
+import { useCommonDataAppSelector, useTrackLayoutAppSelector } from 'store/hooks';
 import styles from 'app-bar/app-bar.scss';
 import { useTranslation } from 'react-i18next';
+import { ExclamationPoint } from 'geoviite-design-lib/exclamation-point/exclamation-point';
+import { useLoader } from 'utils/react-utils';
+import { getCurrentPublicationFailure } from 'ratko/ratko-api';
+import { getChangeTimes } from 'common/change-time-api';
 
 export const FrontpageLink: React.FC = () => {
     const { t } = useTranslation();
@@ -15,6 +19,19 @@ export const FrontpageLink: React.FC = () => {
         (state) => state.selection.publicationSearch,
     );
 
+    const ratkoStatus = useCommonDataAppSelector((state) => state.ratkoStatus);
+    const changeTimes = getChangeTimes();
+
+    const currentRatkoPushError = useLoader(
+        () => getCurrentPublicationFailure(),
+        [changeTimes.publication, changeTimes.ratkoPush],
+    );
+
+    const ratkoIsOffline = ratkoStatus?.connectionStatus === 'OFFLINE';
+    const ratkoNotConfigured = ratkoStatus?.connectionStatus === 'NOT_CONFIGURED';
+    const hasRatkoPushError = !!currentRatkoPushError;
+    const hasSomeError = ratkoIsOffline || ratkoNotConfigured || hasRatkoPushError;
+
     function getFrontpageLink(): string {
         if (selectedPublicationId) {
             return `/publications/${selectedPublicationId}`;
@@ -25,6 +42,18 @@ export const FrontpageLink: React.FC = () => {
         return `/`;
     }
 
+    const ratkoErrorIndicatorTitle = () => {
+        if (hasRatkoPushError) {
+            return 'push-error';
+        } else if (ratkoIsOffline) {
+            return 'ratko-unreachable.';
+        } else if (ratkoNotConfigured) {
+            return 'ratko-not-configured';
+        } else {
+            return undefined;
+        }
+    };
+
     return (
         <NavLink
             to={getFrontpageLink()}
@@ -33,6 +62,15 @@ export const FrontpageLink: React.FC = () => {
             }
             end>
             {t('app-bar.frontpage')}
+            {hasSomeError && (
+                <React.Fragment>
+                    <span
+                        className={styles['app-bar__link--exclamation-point']}
+                        title={t(`app-bar.ratko-errors.${ratkoErrorIndicatorTitle()}`)}>
+                        <ExclamationPoint />
+                    </span>
+                </React.Fragment>
+            )}
         </NavLink>
     );
 };
