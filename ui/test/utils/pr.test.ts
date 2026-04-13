c24eb5a98b7a3289cb3b3d41a8d91ceec0dad8d3
Jyrki Jakobsson <jyrki.jakobsson@solita.fi>
2026-04-13 13:00:04 +0300

    GVT-3441 See that it works when tests pass

--- ui/test/utils/pr.test.ts
+++ ui/test/utils/pr.test.ts
@@ -2,6 +2,6 @@ import { describe, expect, test } from '@jest/globals';
 
 describe('pr test', () => {
     test('check that pr runs ui tests', () => {
-        expect(1).toBe(2);
+        expect(1).toBe(1);
     });
 });
