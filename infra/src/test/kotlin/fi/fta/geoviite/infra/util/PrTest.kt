512711b53fb16f5da15e85fd56d5b62bcb51bc67
Jyrki Jakobsson <jyrki.jakobsson@solita.fi>
2026-04-13 12:52:25 +0300

    GVT-3441 Test PR test-runs in github

---
+++ infra/src/test/kotlin/fi/fta/geoviite/infra/util/PrTest.kt
@@ -0,0 +1,11 @@
+package fi.fta.geoviite.infra.util
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.fail
+
+class PrTest {
+    @Test
+    fun `New test just to check what happens on PR`() {
+        fail { "Test fail to see PR test-run" }
+    }
+}
---
+++ ui/test/utils/pr.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, test } from '@jest/globals';
+
+describe('pr test', () => {
+    test('check that pr runs ui tests', () => {
+        expect(1).toBe(2);
+    });
+});
