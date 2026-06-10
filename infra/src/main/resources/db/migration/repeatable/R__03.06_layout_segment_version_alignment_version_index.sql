48e9853be62c4e6f19cfa2e23764d8559470e30b
Jyrki Jakobsson <jyrki.jakobsson@solita.fi>
2026-06-10 16:34:45 +0300

    GVT-3637 Remove reference_line stuff from old repeatables

--- infra/src/main/resources/db/migration/repeatable/R__03.06_layout_segment_version_alignment_version_index.sql
+++ infra/src/main/resources/db/migration/repeatable/R__03.06_layout_segment_version_alignment_version_index.sql
@@ -1,2 +1,2 @@
--- Old redundant index
+-- No longer relevant: layout.segment_version table was removed in V148_03
 drop index if exists layout.layout_segment_version_alignment_version_index;
--- infra/src/main/resources/db/migration/repeatable/R__03.12.layout_segment_version_geometry_alignment_version_index.sql
+++ infra/src/main/resources/db/migration/repeatable/R__03.12.layout_segment_version_geometry_alignment_version_index.sql
@@ -1,2 +1,2 @@
+-- No longer relevant: layout.segment_version table was removed in V148_03
 drop index if exists layout.segment_version_geometry_alignment_version_ix;
-create index segment_version_geometry_alignment_version_ix on layout.segment_version (geometry_alignment_id, alignment_version);
