85f8dce1ddf6857f9541ee786285000e6dc70d20
Jyrki Jakobsson <jyrki.jakobsson@solita.fi>
2026-06-15 18:23:16 +0300

    GVT-3637 Fix segment->geometry index

--- infra/src/main/resources/db/migration/repeatable/R__03.04_layout_segment_index.sql
+++ infra/src/main/resources/db/migration/repeatable/R__03.04_layout_segment_index.sql
@@ -3,8 +3,8 @@ drop index if exists layout.layout_segment_version_switch;
 
 drop index if exists layout.layout_segment_geometry_element;
 drop index if exists layout.layout_segment_version_geometry_element;
-create index layout_segment_version_geometry_element on layout.segment_version (geometry_alignment_id, geometry_element_index);
+drop index if exists layout.layout_track_number_version_segment_geometry_element;
+create index layout_track_number_version_segment_geometry_element on layout.track_number_version_segment (geometry_alignment_id, geometry_element_index);
 
 drop index if exists layout.layout_segment_bounding_box_index;
 drop index if exists layout.layout_segment_version_segment_geometry;
-create index layout_segment_version_segment_geometry on layout.segment_version (geometry_id);
