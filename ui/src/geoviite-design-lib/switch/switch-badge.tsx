cbf3b291e1011ddcd7f94648acd9b0fa295be2de
Susanna Sorvali <4125605+TygerC@users.noreply.github.com>
2026-07-27 19:35:32 +0300

    GVT-3490: Add SwitchBadgeLink ID+context variant

--- ui/src/geoviite-design-lib/switch/switch-badge.tsx
+++ ui/src/geoviite-design-lib/switch/switch-badge.tsx
@@ -1,8 +1,13 @@
 import * as React from 'react';
 import styles from './switch-badge.scss';
-import { LayoutSwitch } from 'track-layout/track-layout-model';
+import { LayoutSwitch, LayoutSwitchId } from 'track-layout/track-layout-model';
 import { createClassName } from 'vayla-design-lib/utils';
 import { IconColor, Icons, IconSize } from 'vayla-design-lib/icon/Icon';
+import { LayoutContext, TimeStamp } from 'common/common-model';
+import { useSwitch } from 'track-layout/track-layout-react-utils';
+import { trackLayoutActionCreators as TrackLayoutActions } from 'track-layout/track-layout-slice';
+import { createDelegates } from 'store/store-utils';
+import { Spinner } from 'vayla-design-lib/spinner/spinner';
 
 type SwitchBadgeProps = {
     switchItem: LayoutSwitch;
@@ -38,5 +43,41 @@ export const SwitchBadge: React.FC<SwitchBadgeProps> = ({
             <Icons.Switch size={IconSize.SMALL} color={IconColor.INHERIT} />
             <span>{switchItem.name}</span>
         </span>
+    );
+};
+
+type SwitchBadgeLinkProps = {
+    switchId: LayoutSwitchId;
+    layoutContext: LayoutContext;
+    changeTime: TimeStamp;
+    onClick?: (switchId: LayoutSwitchId) => void;
+    status?: SwitchBadgeStatus;
+};
+
+export const SwitchBadgeLink: React.FC<SwitchBadgeLinkProps> = ({
+    switchId,
+    layoutContext,
+    changeTime,
+    onClick,
+    status,
+}: SwitchBadgeLinkProps) => {
+    const switchItem = useSwitch(switchId, layoutContext, changeTime);
+
+    const clickAction = React.useCallback(() => {
+        if (onClick) {
+            onClick(switchId);
+        } else {
+            const delegates = createDelegates(TrackLayoutActions);
+            delegates.onSelect({
+                switches: [switchId],
+                selectedTab: { id: switchId, type: 'SWITCH' },
+            });
+        }
+    }, [onClick, switchId]);
+
+    return switchItem ? (
+        <SwitchBadge switchItem={switchItem} onClick={clickAction} status={status} />
+    ) : (
+        <Spinner />
     );
 };
