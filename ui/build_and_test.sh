9faf2295f04a6d8d1532d6b76a52f8870db8b095
Susanna Sorvali <4125605+TygerC@users.noreply.github.com>
2026-07-30 14:28:09 +0300

    GVT-2970: Add build_and_test.sh that runs Jest before building (#2509)

---
+++ ui/build_and_test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+cd $(dirname $0)
+
+BUILD_TIMESTAMP=$(date +%Y-%m-%d_%H-%M)
+
+echo "Sourcing NVM script..."
+set +e
+source "${HOME}/.nvm/nvm.sh"
+echo "Activating NVM in directory..."
+nvm use
+set -e
+
+echo "Using latest NPM..."
+nvm install --latest-npm
+
+echo "Installing NPM dependencies..."
+npm ci
+
+echo "Running Jest tests..."
+npm test -- --ci
+
+echo "Building frontend..."
+npm run build --
+cp ../LICENSE.txt dist/
