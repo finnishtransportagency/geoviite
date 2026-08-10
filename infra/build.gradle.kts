4312ce4fdaba5c078c9f00c4dd304c0da29409ab
Jyrki Jakobsson <jyrki.jakobsson@solita.fi>
2026-08-10 12:51:55 +0300

    General: move checker-qual dep closer to postgresql & maki it compileOnly

--- infra/build.gradle.kts
+++ infra/build.gradle.kts
@@ -95,8 +95,6 @@ dependencies {
     implementation("com.zaxxer:HikariCP:7.1.0")
     implementation("org.flywaydb:flyway-core:11.20.3")
     implementation("org.flywaydb:flyway-database-postgresql:11.20.3")
-    // Used by postgresql driver as runtime, but kotlin needs it at compile-time for nullability annotations
-    implementation("org.checkerframework:checker-qual:3.55.1")
     implementation("com.github.ben-manes.caffeine:caffeine:3.2.4")
     implementation("org.geotools:gt-main:$geotoolsVersion") {
         // Excluded as the license (JDL or JRL) compatibility is unconfirmed. We don't need this.
@@ -116,6 +114,8 @@ dependencies {
     implementation("com.auth0:java-jwt:4.6.0")
     implementation("io.netty:netty-resolver-dns-native-macos:4.2.10.Final:osx-aarch_64")
     implementation("org.postgresql:postgresql:42.7.13")
+    // Used by postgresql driver as runtime, but kotlin needs it at compile-time for nullability annotations
+    compileOnly("org.checkerframework:checker-qual:3.55.1")
     implementation("jakarta.activation:jakarta.activation-api:2.1.4")
     implementation("jakarta.xml.bind:jakarta.xml.bind-api:4.0.5")
     implementation("com.github.davidmoten:rtree2:0.9.3")
