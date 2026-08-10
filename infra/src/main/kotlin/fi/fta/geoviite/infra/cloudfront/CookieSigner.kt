b013018395ca9637034f08601350127246e5a93a
Jyrki Jakobsson <jyrki.jakobsson@solita.fi>
2026-08-10 10:35:26 +0300

    General: Reformat file via cmd ktfmt

--- infra/src/main/kotlin/fi/fta/geoviite/infra/cloudfront/CookieSigner.kt
+++ infra/src/main/kotlin/fi/fta/geoviite/infra/cloudfront/CookieSigner.kt
@@ -2,14 +2,14 @@ package fi.fta.geoviite.infra.cloudfront
 
 import fi.fta.geoviite.infra.SpringContextUtility
 import fi.fta.geoviite.infra.cloudfront.KeyUtils.Companion.parseBase64DerToPrivateKey
-import org.slf4j.LoggerFactory
-import org.springframework.stereotype.Component
-import software.amazon.awssdk.services.cloudfront.CloudFrontUtilities
-import software.amazon.awssdk.services.cloudfront.model.CustomSignerRequest
 import java.text.SimpleDateFormat
 import java.time.Duration
 import java.time.Instant
 import java.util.*
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
+import software.amazon.awssdk.services.cloudfront.CloudFrontUtilities
+import software.amazon.awssdk.services.cloudfront.model.CustomSignerRequest
 
 @Component
 class CookieSigner {
