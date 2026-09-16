ef9609d8d89187dd0738260bddce2de409128e63
Susanna Sorvali <4125605+TygerC@users.noreply.github.com>
2026-09-16 16:55:01 +0300

    GVT-3711 Disable design collection IT tests in ExtTrackNumberCollectionIT

--- infra/src/test/kotlin/fi/fta/geoviite/api/tracklayout/v1/ExtTrackNumberCollectionIT.kt
+++ infra/src/test/kotlin/fi/fta/geoviite/api/tracklayout/v1/ExtTrackNumberCollectionIT.kt
@@ -24,6 +24,7 @@ import fi.fta.geoviite.infra.tracklayout.trackNumber
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -410,6 +411,7 @@ constructor(
         }
     }
 
+    @Disabled
     @Test
     fun `Design collection route lists only track numbers with an OID in the design`() {
         val (inDesignId, inDesignOid) =
@@ -444,6 +446,7 @@ constructor(
         }
     }
 
+    @Disabled
     @Test
     fun `Track number change published in a design is only visible in the design collection modifications`() {
         val (tnId, oid) =
